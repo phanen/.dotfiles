8a1a57eae00cb85d022207e7517cc4cfcc2c5261
phanium <nxhsdu@qq.com>
2023-04-08 10:46:14 +0800

    refactor(snip): mv friendly-snippets to lazy.nvim

--- .config/nvim/lua/plugins/snip.lua
+++ .config/nvim/lua/plugins/snip.lua
@@ -1,56 +1,55 @@
 return {
-	{
-		'L3MON4D3/LuaSnip',
-		event = 'InsertEnter',
-		-- dependencies = { 'rafamadriz/friendly-snippets' },
-		config = function()
-			local ls = require('luasnip')
-			local types = require('luasnip.util.types')
-			local extras = require('luasnip.extras')
-			local fmt = require('luasnip.extras.fmt').fmt
+  {
+    'L3MON4D3/LuaSnip',
+    event = 'InsertEnter',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    config = function()
+      local ls = require('luasnip')
+      local types = require('luasnip.util.types')
+      local extras = require('luasnip.extras')
+      local fmt = require('luasnip.extras.fmt').fmt
 
-			ls.config.set_config({
-				history = false,
-				region_check_events = 'CursorMoved,CursorHold,InsertEnter',
-				delete_check_events = 'InsertLeave',
-				ext_opts = {
-					[types.choiceNode] = {
-						active = {
-							hl_mode = 'combine',
-							virt_text = { { '●', 'Operator' } },
-						},
-					},
-					[types.insertNode] = {
-						active = {
-							hl_mode = 'combine',
-							virt_text = { { '●', 'Type' } },
-						},
-					},
-				},
-				enable_autosnippets = true,
-				snip_env = {
-					fmt = fmt,
-					m = extras.match,
-					t = ls.text_node,
-					f = ls.function_node,
-					c = ls.choice_node,
-					d = ls.dynamic_node,
-					i = ls.insert_node,
-					l = extras.lamda,
-					snippet = ls.snippet,
-				},
-			})
+      ls.config.set_config({
+        history = false,
+        region_check_events = 'CursorMoved,CursorHold,InsertEnter',
+        delete_check_events = 'InsertLeave',
+        ext_opts = {
+          [types.choiceNode] = {
+            active = {
+              hl_mode = 'combine',
+              virt_text = { { '●', 'Operator' } },
+            },
+          },
+          [types.insertNode] = {
+            active = {
+              hl_mode = 'combine',
+              virt_text = { { '●', 'Type' } },
+            },
+          },
+        },
+        enable_autosnippets = true,
+        snip_env = {
+          fmt = fmt,
+          m = extras.match,
+          t = ls.text_node,
+          f = ls.function_node,
+          c = ls.choice_node,
+          d = ls.dynamic_node,
+          i = ls.insert_node,
+          l = extras.lamda,
+          snippet = ls.snippet,
+        },
+      })
 
-			require('luasnip.loaders.from_lua').lazy_load()
-			require('luasnip.loaders.from_vscode').lazy_load()
-			-- load vscode-like snippets
-			require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets/vscode-like" } })
-		end
-	},
+      require('luasnip.loaders.from_lua').lazy_load()
+      require('luasnip.loaders.from_vscode').lazy_load()
+      ls.filetype_extend("all", { "_" })
+    end
+  },
 
-	-- {
-	-- 	'benfowler/telescope-luasnip.nvim',
-	-- 	dependencies = { 'nvim-telescope/telescope.nvim' },
-	-- 	config = function() require('telescope').load_extension('luasnip') end,
-	-- },
+  -- {
+  -- 	'benfowler/telescope-luasnip.nvim',
+  -- 	dependencies = { 'nvim-telescope/telescope.nvim' },
+  -- 	config = function() require('telescope').load_extension('luasnip') end,
+  -- },
 }
