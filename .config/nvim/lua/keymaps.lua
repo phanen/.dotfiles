0c5b4f2e266428ca159514f1d5fe2aea1360f324
phanium <nxhsdu@qq.com>
2023-04-09 11:00:06 +0800

    feat: new keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -18,7 +18,6 @@ map({ 'n', 'v', 'i' }, '<right>', '<nop>')
 map({ 'n', 'v', 'i' }, '<up>', '<nop>')
 map({ 'n', 'v', 'i' }, '<down>', '<nop>')
 map({ 'n', 'v' }, '<space>', '<nop>')
-map({ 'n', 'v' }, '<space>f', '<nop>')
 
 nnoremap('k', 'v:count == 0 ? "gk" : "k"', { expr = true })
 nnoremap('j', 'v:count == 0 ? "gj" : "j"', { expr = true })
@@ -27,76 +26,74 @@ nnoremap('<c-d>', '<c-d>zz')
 vnoremap('<', '<gv')
 vnoremap('>', '>gv')
 vnoremap('p', '"_dp')
-
--- word wrap
-
--- text motion
 nnoremap('<a-j>', ':m .+1<cr>')
 nnoremap('<a-k>', ':m .-2<cr>')
 
--- config
-nnoremap('<leader>cc', '<cmd>tabnew $XDG_CONFIG_HOME/nvim<cr>')
-nnoremap('<leader>cs', '<cmd>source $XDG_CONFIG_HOME/nvim/init.lua<cr>')
+nnoremap('<c-p>', '<c-y>')
+nnoremap('<c-n>', '<c-e>')
 
+nnoremap('<leader>C', '<cmd>edit $XDG_CONFIG_HOME/nvim<cr>')
 
--- switch buffers
+-- navigation
 nnoremap('<c-f>', '<cmd>BufferLineCycleNext<cr>')
 nnoremap('<c-b>', '<cmd>BufferLineCyclePrev<cr>')
 
--- switch windows
 nnoremap('<c-h>', '<cmd>wincmd h<cr>')
 nnoremap('<c-j>', '<cmd>wincmd j<cr>')
 nnoremap('<c-k>', '<cmd>wincmd k<cr>')
 nnoremap('<c-l>', '<cmd>wincmd l<cr>')
 
--- resize windows
+nnoremap('<leader>tn', '<cmd>tabnew<cr>')
+
+-- layout
 nnoremap('<c-up>', '<cmd>resize -2<cr>')
 nnoremap('<c-down>', '<cmd>resize +2<cr>')
 nnoremap('<c-left>', '<cmd>vertical resize -2<cr>')
 nnoremap('<c-right>', '<cmd>vertical resize +2<cr>')
 
--- tab new
-nnoremap('<leader>tn', '<cmd>tabnew<cr>')
-
--- ls
-nnoremap('<leader>ls', '<cmd>ls<cr>')
-
 -- get img
 nnoremap('<leader>gi', '<cmd>UploadClipboard<cr>')
 
 -- symbol rename
-vim.keymap.set('n', '<leader>rn', ':IncRename ')
+nnoremap('<leader>rn', ':IncRename ')
 
 -- style
 -- 全角2半角
 -- TODO refactor in lua
 nnoremap('<leader>rp', require('utils').get_full2half_vimcmd())
-nnoremap('<leader>rs', '<cmd>%s/\\s*$//g<cr>', { desc = 'clean head space'})
-nnoremap('<leader>rh', '<cmd>g/^$/d<cr>', { desc = 'clean tail space'})
-nnoremap('<leader>rs', '<cmd>%s/\\s*$//g<cr>', { desc = 'swap quote make'})
+nnoremap('<leader>rs', '<cmd>%s/\\s*$//g<cr>', { desc = 'clean tail space'})
+nnoremap('<leader>rl', '<cmd>g/^$/d<cr>', { desc = 'clean the blank line'})
+-- TODO (complete comment char)
+nnoremap('<leader>rc', '<cmd>g/^#/d<cr>', { desc = 'clean the comment line'})
+
+-- how to quit in vim
+nnoremap('<leader>q', '<cmd>bdelete!<cr>')
+nnoremap('<localleader>q', '<cmd>quit!<cr>')
+nnoremap('<localleader>w', '<cmd>write<cr>')
+nnoremap('<localleader>w', '<cmd>write<cr>')
+nnoremap('X', '<cmd>bdelete!<cr>')
+inoremap('<c-q>', '<cmd>bdelete!<cr>')
+inoremap('<c-s>', '<cmd>write<cr>')
+
+-- toggle windows
+nnoremap('<leader>wn', '<cmd>NvimTreeFindFileToggle<cr>')
+nnoremap('<leader>wo', '<cmd>AerialToggle<cr>')
+nnoremap('<leader>wm', '<cmd>MarkdownPreview<cr>')
+nnoremap('<leader>wl', '<cmd>Lazy<cr>')
+
+-- toggle options
+nnoremap('<leader>of', '<cmd>set foldenable!<cr>')
+nnoremap('<leader>ob', require('utils.themes').toggle_bg, { desc = 'toggle background'} )
+nnoremap('<leader><tab>', require('utils').next_colorscheme, { desc = 'switch colorscheme'})
+nnoremap('<leader>ls!', '<cmd>ls<cr>')
+
+-- toy term
+nnoremap('<m-t>', ':split term://bash<CR>i')
+tnoremap('<esc>', '<c-\\><c-n>')
+tnoremap('<m-t>', '<c-\\><c-n>:bdelete! %<CR>')
 
 -- diagnostic
 nnoremap('[d', vim.diagnostic.goto_prev)
 nnoremap(']d', vim.diagnostic.goto_next)
 nnoremap('<leader>df', vim.diagnostic.open_float)
 nnoremap('<leader>ds', vim.diagnostic.setloclist)
-
--- toggle windows
-nnoremap('<leader>q', '<cmd>bdelete!<cr>')
-nnoremap('<localleader>q', '<cmd>wincmd q<cr>')
-nnoremap('<localleader>  ', '<cmd>w<cr>')
-
-nnoremap('<leader>wr', '<cmd>NvimTreeToggle<cr>')
-nnoremap('<leader>wn', '<cmd>NvimTreeToggle<cr>')
-nnoremap('<leader>wa', '<cmd>AerialToggle<cr>')
-nnoremap('<leader>wm', '<cmd>MarkdownPreview<cr>')
-
--- toggle options
-nnoremap('<leader>of', '<cmd>set foldenable!<cr>')
-nnoremap('<leader>ob', require('utils.themes').toggle_bg, { desc = 'toggle background'} )
-nnoremap('<leader><tab>', require('utils').next_colorscheme, { desc = 'switch colorscheme'})
-
--- toy term
-nnoremap('<m-t>', ':split term://bash<CR>i')
-tnoremap('<esc>', '<c-\\><c-n>')
-tnoremap('<m-t>', '<c-\\><c-n>:bdelete! %<CR>')
