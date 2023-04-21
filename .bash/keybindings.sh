54f5fc8aa0410e227dc6e0917a6be1aac5932216
phanium <nxhsdu@qq.com>
2023-04-21 23:54:58 +0800

    feat: `esc` as a leader key

--- .bash/keybindings.sh
+++ .bash/keybindings.sh
@@ -1,13 +1,15 @@
-
-## key bindings
-bind '"\C-o":"\C-alfcd \C-e"'
-bind '"\C-t":"$(compgen -c | fzf)"'
-bind '"\C-v": "\C-avi \C-e"'
-bind '"\es": "\C-asudo \C-e"'
-bind '"\e\C-b": "\C-e > /dev/null 2>&1 &"'
-bind '"\e\C-l": "\C-e | less"'
 run-help() { help "$READLINE_LINE" 2>/dev/null || man "$READLINE_LINE"; }
-bind -m vi-insert -x '"\eh": run-help'
-bind -m emacs -x     '"\eh": run-help'
 
-bind '"\C-l": "\C-e\C-uclear\C-m"'
+bind '"\eq": "\C-e > /dev/null 2>&1 &"'
+bind '"\ew": "\C-asudo \C-e"'
+bind '"\ee": "\C-anvim \C-e"'
+
+bind '"\eo":"\C-alfcd \C-e"'
+bind '"\et":"$(compgen -c | fzf)"'
+
+bind '"\eg": "2>&1 | rg "'
+bind -m vi-insert -x '"\eh": run-help'
+bind -m emacs -x '"\eh": run-help'
+bind '"\ej": "2>&1 | nvim -"'
+bind '"\ek": "\C-e\C-uclear\C-m"'
+bind '"\el": "\C-e | bat"'
