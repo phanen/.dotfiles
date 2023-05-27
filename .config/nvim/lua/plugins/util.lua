6507d1e95345d2927ffcbd08387d6d46c645effe
phanium <nxhsdu@qq.com>
2023-05-27 19:32:42 +0800

    perf: remove fold-cycle (why use it since there's builtin)

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,12 +1,13 @@
 return {
   -- toggle fold
-  {
-    'jghauser/fold-cycle.nvim',
-    config = true,
-    keys = {
-      { '<BS>', function() require('fold-cycle').open() end, desc = 'fold-cycle: toggle' },
-    },
-  },
+  -- {
+  --   'jghauser/fold-cycle.nvim',
+  --   config = true,
+  --   keys = {
+  --     { '<leader>of', function() require('fold-cycle').open() end, desc = 'fold-cycle: toggle' },
+  --   },
+  -- },
+
   -- diff arbitrary blocks of text with each other
   { 'AndrewRadev/linediff.vim', cmd = 'Linediff' },
 }
