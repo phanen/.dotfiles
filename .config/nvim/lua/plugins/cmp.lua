10bc5c72b7166923e7c1f589019ab193cd35ab5c
phanium <nxhsdu@qq.com>
2023-05-28 21:57:39 +0800

    style: format it, horrible

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,27 +1,24 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-cmdline',
-      'saadparwaiz1/cmp_luasnip',
-      { 'f3fora/cmp-spell', ft = { 'gitcommit', 'markdown', 'norg', 'org' } },
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      { "f3fora/cmp-spell", ft = { "gitcommit", "markdown", "norg", "org" } },
     },
 
-
-
-    config = function ()
-
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
+    config = function()
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      local lspkind = require "lspkind"
 
       local formatting = {}
-      formatting.format = lspkind.cmp_format({
+      formatting.format = lspkind.cmp_format {
         mode = "symbol",
         symbol_map = {
           Copilot = " ",
@@ -58,27 +55,38 @@ return {
           path = "[path]",
           luasnip = "[snip]",
         },
-      })
+      }
 
       cmp.setup {
-        enabled = function() return vim.api.nvim_buf_get_option(0, 'modifiable') and vim.bo.buftype ~= 'prompt' end,
+        enabled = function() return vim.api.nvim_buf_get_option(0, "modifiable") and vim.bo.buftype ~= "prompt" end,
         preselect = cmp.PreselectMode.None,
         mapping = {
-          ['<c-e>'] = cmp.mapping.abort(),
-          ['<c-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "c"}),
-          ['<c-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "c"}),
-          ["<c-space>"] = cmp.mapping.complete({}),
-          ['<cr>'] = cmp.mapping(cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false, }), {"i"}),
-          ['<tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-            else fallback() end
-          end, { 'i', 's' }),
-          ['<s-tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-            else fallback() end
-          end, { 'i', 's' }),
+          ["<c-e>"] = cmp.mapping.abort(),
+          ["<c-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+          ["<c-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+          ["<c-space>"] = cmp.mapping.complete {},
+          ["<cr>"] = cmp.mapping(
+            cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
+            { "i" }
+          ),
+          ["<tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<s-tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         },
         -- mapping = cmp.mapping.preset.insert {
         --   ['<c-d>'] = cmp.mapping.scroll_docs(-4),
@@ -91,24 +99,23 @@ return {
         },
 
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'nvim_lua' },
-          { name = 'path' },
-          { name = 'buffer' },
-          { name = 'spell',
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "nvim_lsp_signature_help" },
+          { name = "nvim_lua" },
+          { name = "path" },
+          { name = "buffer" },
+          {
+            name = "spell",
             option = {
               -- if include vim.fn.spellsuggest("str")
               keep_all_entries = false,
               -- enable if certain area
               -- `:help treesitter-highlight-spell`
-              enable_in_context = function()
-                return true
-              end,},
+              enable_in_context = function() return true end,
+            },
           },
         },
-
 
         formatting = formatting,
         -- {
@@ -126,7 +133,6 @@ return {
         --   end,
         -- },
       }
-
-    end
+    end,
   },
 }
