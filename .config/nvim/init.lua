64025042614fabd4f9087b501a4898ed206c020b
phanium <nxhsdu@qq.com>
2023-05-29 13:07:06 +0800

    fix: pcall debug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,28 @@
 local g, fn, opt, loop, env, cmd = vim.g, vim.fn, vim.opt, vim.loop, vim.env, vim.cmd
 
 -- g.os = loop.os_uname().sysname
--- g.open_cmd = g.os == 'Linux' and 'xdg-open' or 'open'
 
-g.mapleader = ' '
-g.maplocalleader = ','
+g.mapleader = " "
+g.maplocalleader = ","
 
-require('keymaps')
-require('settings')
-require('lazyman')
+for _, source in ipairs {
+  "keymaps",
+  "settings",
+  "pacman",
+} do
+  local ok, fault = pcall(require, source)
+  if not ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
 
 -- filter down a quickfix list
-cmd.packadd('cfilter')
+cmd.packadd "cfilter"
 
-
-local cur_hour = tonumber(fn.system('date +%H'))
-if env.TERM == 'linux' then
-  g.colorscheme = 'default'
+local cur_hour = tonumber(fn.system "date +%H")
+if env.TERM == "linux" then
+  g.colorscheme = "default"
 elseif cur_hour >= 18 or cur_hour <= 6 then
-  g.colorscheme = 'kanagawa-wave'
+  g.colorscheme = "kanagawa-wave"
 else
-  g.colorscheme = 'kanagawa-lotus'
+  g.colorscheme = "kanagawa-lotus"
 end
-vim.cmd('colorscheme ' .. 'kanagawa-lotus')
+vim.cmd("colorscheme " .. "kanagawa-lotus")
