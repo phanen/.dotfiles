251f3b1f9c80f8a2c3ec9b217485e23965ca2fb5
phanium <nxhsdu@qq.com>
2023-05-29 13:00:19 +0800

    feat(kmonad): show mode in polybar

--- .config/kmonad/keypress.sh
+++ .config/kmonad/keypress.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-polybar-msg action readline module_toggle
+# TODO: status machine
+
+hookid=${1:-0}
+case $1 in
+  qwe       ) hookid=0;;
+  rdl       ) hookid=1;;
+  cmk       ) hookid=2;;
+  cps       ) hookid=3;;
+  num       ) hookid=4;;
+esac
+
+polybar-msg action kmonad hook ${hookid}
