14894e0c8779a285a038d8fc89f08b4627c8b1aa
phanium <nxhsdu@qq.com>
2023-05-29 13:06:46 +0800

    style: format by stylua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,5 @@
-local mp = require('utils.keymaps')
-local ac = require('utils.actions')
+local mp = require "utils.keymaps"
+local ac = require "utils.actions"
 
 local map = mp.map
 local nmap = mp.nmap
@@ -17,123 +17,124 @@ local lmap = mp.lmap
 -- which-key will intercept
 -- map( { 'n', 'v', 'o' }, '<space>', '<nop>')
 
-nmap('k', 'v:count == 0 ? "gk" : "k"', { expr = true })
-nmap('j', 'v:count == 0 ? "gj" : "j"', { expr = true })
+nmap("k", 'v:count == 0 ? "gk" : "k"', { expr = true })
+nmap("j", 'v:count == 0 ? "gj" : "j"', { expr = true })
 
-nmap('<c-u>', '<c-u>zz')
-nmap('<c-d>', '<c-d>zz')
+nmap("<c-u>", "<c-u>zz")
+nmap("<c-d>", "<c-d>zz")
 
-vmap('<', '<gv')
-vmap('>', '>gv')
-vmap(',', '<gv')
-vmap('.', '>gv')
+vmap("<", "<gv")
+vmap(">", ">gv")
+vmap(",", "<gv")
+vmap(".", ">gv")
 
-vmap('p', '"_dp')
+vmap("p", '"_dp')
 
-nmap('<a-k>', ':m .-2<cr>')
-nmap('<a-j>', ':m .+1<cr>')
+nmap("<a-k>", ":m .-2<cr>")
+nmap("<a-j>", ":m .+1<cr>")
 
 -- https://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
-cmap('w!!', 'w !sudo tee > /dev/null')
+cmap("w!!", "w !sudo tee > /dev/null")
 
-vmap('d', '"_d')
-vmap('c', '"_c')
-nmap('c', '"_c')
-nmap('d', '"_d')
-nmap('C', '"_C')
-nmap('D', '"_D')
-nmap('X', 'D')
+vmap("d", '"_d')
+vmap("c", '"_c')
+nmap("c", '"_c')
+nmap("d", '"_d')
+nmap("C", '"_C')
+nmap("D", '"_D')
+nmap("X", "D")
 
-nmap('<leader>z', '<Plug>(comment_toggle_linewise_count)')
+nmap("<leader>z", "<Plug>(comment_toggle_linewise_count)")
 -- nnoremap('<leader>C', '<cmd>edit $XDG_CONFIG_HOME/nvim<cr>')
 
 -- navigation
-nmap('<c-f>', '<cmd>BufferLineCycleNext<cr>')
-nmap('<c-b>', '<cmd>BufferLineCyclePrev<cr>')
+nmap("<c-f>", "<cmd>BufferLineCycleNext<cr>")
+nmap("<c-b>", "<cmd>BufferLineCyclePrev<cr>")
 
-nmap('<c-j>', '<cmd>wincmd w<cr>')
-nmap('<c-k>', '<cmd>wincmd W<cr>')
+nmap("<c-j>", "<cmd>wincmd w<cr>")
+nmap("<c-k>", "<cmd>wincmd W<cr>")
 
-map({ 'n', 'v', 'o' }, '<c-h>', 'g^')
-map({ 'n', 'v', 'o' }, '<c-l>', 'g$')
+map({ "n", "v", "o" }, "<c-h>", "g^")
+map({ "n", "v", "o" }, "<c-l>", "g$")
 
 -- layout
-nmap('<c-up>', '<cmd>resize -2<cr>')
-nmap('<c-down>', '<cmd>resize +2<cr>')
-nmap('<c-left>', '<cmd>vertical resize -2<cr>')
-nmap('<c-right>', '<cmd>vertical resize +2<cr>')
-
--- get img
-nmap('<leader>gi', '<cmd>UploadClipboard<cr>')
+-- nmap("<c-k>", "<cmd>resize -2<cr>")
+-- nmap("<c-j>", "<cmd>resize +2<cr>")
+-- nmap("<c-h>", "<cmd>vertical resize -2<cr>")
+-- nmap("<c-l>", "<cmd>vertical resize +2<cr>")
 
 -- symbol rename
-nmap('<leader>rn', ':IncRename ')
+nmap("<leader>rn", ":IncRename ")
 
 -- subtitution
--- TODO refactor in lua
--- nmap(
---   '<leader>rp',
---   [[
---         <cmd>%s/[，、（）［］｛｝＜＞？／；。！“”：　]/\={'，':', ', '、':', ', '（':'(', '）':')', '［':'[', '］':']', '｛':'{', '｝':'}', '＜':'<', '＞':'>', '？':'? ', '／':'\/', '；':'; ', '。':'. ', '：': ': ', '！': '! ', '”': '"', '“': '"', '　':'  '}[submatch(0)]/g<cr>
---     ]],
--- )
+nmap(
+  "<leader>rp",
+  [[
+        <cmd>%s/[，、（）［］｛｝＜＞？／；。！“”：　]/\={'，':', ', '、':', ', '（':'(', '）':')', '［':'[', '］':']', '｛':'{', '｝':'}', '＜':'<', '＞':'>', '？':'? ', '／':'\/', '；':'; ', '。':'. ', '：': ': ', '！': '! ', '”': '"', '“': '"', '　':'  '}[submatch(0)]/g<cr>
+    ]],
+  { desc = "half to full" }
+)
 
-nmap('<leader>rs', "<cmd>%s/\\s*$//g<cr>''", { desc = 'clean tail space' })
-nmap('<leader>rl', "<cmd>g/^$/d<cr>''", { desc = 'clean the blank line' })
+nmap("<leader>rs", "<cmd>%s/\\s*$//g<cr>''", { desc = "clean tail space" })
+nmap("<leader>rl", "<cmd>g/^$/d<cr>''", { desc = "clean the blank line" })
 -- TODO (complete comment char)
-nmap('<leader>rc', "<cmd>g/^#/d<cr>''", { desc = 'clean the comment line' })
-vmap('<leader>rk', [[<cmd>'<,'>s/\/\* \(.*\) \*\//\/\/ \1/g<cr>]])
-vmap('<leader>r,', [[<cmd>'<,'>s/,\([^ ]\)/, \1/g<cr>]])
+nmap("<leader>rc", "<cmd>g/^#/d<cr>''", { desc = "clean the comment line" })
+vmap("<leader>rk", [[<cmd>'<,'>s/\/\* \(.*\) \*\//\/\/ \1/g<cr>]])
+vmap("<leader>r,", [[<cmd>'<,'>s/,\([^ ]\)/, \1/g<cr>]])
 
 -- hex to dec
-vmap('<leader>ro', [[<cmd>'<,'>s/0x[0-9a-fA-F]\+/\=str2nr(submatch(0), 16)<cr>]])
+vmap("<leader>ro", [[<cmd>'<,'>s/0x[0-9a-fA-F]\+/\=str2nr(submatch(0), 16)<cr>]])
 -- dec to hex
-vmap('<leader>rh', [[<cmd>'<,'>s/\d\+/\=printf("0x%04x", submatch(0))<cr>]])
+vmap("<leader>rh", [[<cmd>'<,'>s/\d\+/\=printf("0x%04x", submatch(0))<cr>]])
 
 -- how to quit in vim
-nmap('<leader>q', '<cmd>Bdelete!<cr>')
-nmap('<localleader>w', '<cmd>write<cr>')
-imap('<c-q>', '<cmd>bdelete!<cr>')
-imap('<c-s>', '<cmd>write<cr>')
+nmap("<leader>q", "<cmd>Bdelete!<cr>")
+nmap("<localleader>w", "<cmd>write<cr>")
+imap("<c-q>", "<cmd>bdelete!<cr>")
+imap("<c-s>", "<cmd>write<cr>")
 
 -- toggle windows
-nmap('<leader>wn', '<cmd>NvimTreeFindFileToggle<cr>')
-nmap('<leader>wo', '<cmd>AerialToggle<cr>')
-nmap('<leader>wm', '<cmd>MarkdownPreview<cr>')
-nmap('<leader>wl', '<cmd>Lazy<cr>')
-nmap('<leader>wj', '<cmd>Navbuddy<cr>')
-nmap('<leader>wi', '<cmd>LspInfo<cr>')
-nmap('<leader>wu', '<cmd>NullLsInfo<cr>')
+nmap("<leader>wn", "<cmd>NvimTreeFindFileToggle<cr>")
+nmap("<leader>wo", "<cmd>AerialToggle<cr>")
+nmap("<leader>wm", "<cmd>MarkdownPreview<cr>")
+nmap("<leader>wl", "<cmd>Lazy<cr>")
+nmap("<leader>wj", "<cmd>Navbuddy<cr>")
+nmap("<leader>wi", "<cmd>LspInfo<cr>")
+nmap("<leader>wu", "<cmd>NullLsInfo<cr>")
+nmap("<leader>wy", "<cmd>Mason<cr>")
 
 -- toggle options
-nmap('<leader>of', '<cmd>set foldenable!<cr>')
-nmap('<leader>os', '<cmd>set spell!<cr>')
-nmap('<leader>on', function()
-  if string.match(vim.o.nrformats, 'alpha') then
-    vim.cmd([[set nrformats-=alpha]])
+nmap("<leader>of", "<cmd>set foldenable!<cr>")
+nmap("<leader>os", "<cmd>set spell!<cr>")
+nmap("<leader>on", function()
+  if string.match(vim.o.nrformats, "alpha") then
+    vim.cmd [[set nrformats-=alpha]]
   else
-    vim.cmd([[set nrformats+=alpha]])
+    vim.cmd [[set nrformats+=alpha]]
   end
-end, { desc = 'toggle nrformats' })
+end, { desc = "toggle nrformats" })
 
-nmap('<leader>oj', '<cmd>wincmd _<cr>')
-nmap('<leader>ok', '<cmd>wincmd =<cr>')
-nmap('<leader>ob', ac.toggle_bg, { desc = 'toggle background' })
-nmap('<leader><tab>', ac.next_colorscheme, { desc = 'switch colorscheme' })
+nmap("<leader>oj", "<cmd>wincmd _<cr>")
+nmap("<leader>ok", "<cmd>wincmd =<cr>")
+nmap("<leader>ob", ac.toggle_bg, { desc = "toggle background" })
+nmap("<leader><tab>", ac.next_colorscheme, { desc = "switch colorscheme" })
 
 -- term
-nmap('<m-t>', ':split term://bash<cr>i')
-tmap('<c-q>', '<c-\\><c-n>')
-tmap('<m-t>', '<c-\\><c-n>:bdelete! %<cr>')
+nmap("<m-t>", ":split term://bash<cr>i")
+tmap("<c-q>", "<c-\\><c-n>")
+tmap("<m-t>", "<c-\\><c-n>:bdelete! %<cr>")
 -- tnoremap('<c-;>', '<cmd>ToggleTerm<cr>') -- unusable in vim
 
 -- diagnostic
-nmap('[d', vim.diagnostic.goto_prev)
-nmap(']d', vim.diagnostic.goto_next)
-nmap('<leader>df', vim.diagnostic.open_float)
-nmap('<leader>ds', vim.diagnostic.setloclist)
+nmap("[d", vim.diagnostic.goto_prev)
+nmap("]d", vim.diagnostic.goto_next)
+nmap("<leader>df", vim.diagnostic.open_float)
+nmap("<leader>ds", vim.diagnostic.setloclist)
 
 -- search
 -- https://vim.fandom.com/wiki/Search_for_visually_selected_text
 -- https://superuser.com/questions/41378/how-to-search-for-selected-text-in-vim
-vmap('//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]])
+vmap("//", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]])
+
+-- get something
+nmap("<leader>gi", "<cmd>UploadClipboard<cr>")
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
-local nmap = require('utils.keymaps').nmap
-local vmap = require('utils.keymaps').vmap
+local nmap = require("utils.keymaps").nmap
+local vmap = require("utils.keymaps").vmap
 
 -- local tel_ex = function(name) return require('telescope').extensions[name] end
 -- local live_grep = function(_) tel_ex('menufacture').live_grep(opts) end
@@ -15,9 +15,10 @@ local vmap = require('utils.keymaps').vmap
 
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
     dependencies = {
-      { 'nvim-lua/plenary.nvim', },
+      { "nvim-lua/plenary.nvim" },
       -- { 'molecule-man/telescope-menufacture' },
       -- { 'natecraddock/telescope-zf-native.nvim' },
     },
@@ -27,75 +28,74 @@ return {
     -- 2. add leader to keys
     -- then leader will be enable again...
 
-    cmd = 'Telescope',
-    keys = { '<leader><leader>', },
+    cmd = "Telescope",
+    keys = { "<leader><leader>" },
     config = function()
-      local tl = require('telescope')
-      local tb = require('telescope.builtin')
+      local tl = require "telescope"
+      local tb = require "telescope.builtin"
 
       tl.setup {
         defaults = {
           mappings = {
-            i = { ['<c-u>'] = false, ['<c-d>'] = false, },
+            i = { ["<c-u>"] = false, ["<c-d>"] = false },
           },
         },
       }
       -- enable fzf native
-      pcall(tl.load_extension, 'fzf')
+      pcall(tl.load_extension, "fzf")
 
-      nmap('<leader>/', function()
-        tb.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { previewer = false, }) end, { desc = 'fzf curbuf' }
+      nmap(
+        "<leader>/",
+        function() tb.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown { previewer = false }) end,
+        { desc = "fzf curbuf" }
       )
-      nmap('<leader>fj', tb.find_files, { desc = 'fzf bufs' })
-      nmap('<leader>fk', tb.live_grep, { desc = 'fzf grep' })
-      nmap('<leader>fl', tb.buffers, { desc = 'fzf buffers' })
+      nmap("<leader>fj", tb.find_files, { desc = "fzf bufs" })
+      nmap("<leader>fk", tb.live_grep, { desc = "fzf grep" })
+      nmap("<leader>fl", tb.buffers, { desc = "fzf buffers" })
       -- nnoremap('<leader>f;;', tb, { desc = 'fzf files' })
 
-      nmap('<leader>fb', tb.buffers, { desc = 'fzf bufs' })
-      nmap('<leader>fr', tb.oldfiles, { desc = 'fzf recents' })
-      nmap('<leader>ff', tb.find_files, { desc = 'fzf files' })
-      nmap('<leader><space>', tb.find_files, { desc = 'fzf files' })
-      nmap('<leader>:', tb.command_history, { desc = 'command' })
-      nmap('<leader>fs', tb.live_grep, { desc = 'fzf grep' })
-      nmap('<leader>fw', tb.grep_string, { desc = 'find word' })
-      nmap('<leader>fd', tb.diagnostics, { desc = 'find diagnostics' })
+      nmap("<leader>fb", tb.buffers, { desc = "fzf bufs" })
+      nmap("<leader>fr", tb.oldfiles, { desc = "fzf recents" })
+      nmap("<leader>ff", tb.find_files, { desc = "fzf files" })
+      nmap("<leader>:", tb.command_history, { desc = "command" })
+      nmap("<leader>fs", tb.live_grep, { desc = "fzf grep" })
+      nmap("<leader>fw", tb.grep_string, { desc = "find word" })
+      nmap("<leader>fd", tb.diagnostics, { desc = "find diagnostics" })
       -- nnoremap('<leader>fh', "<cmd>Telescope heading<cr>", { desc = 'find headings' })
-      nmap('<leader>fp', tb.help_tags, { desc = 'find help' })
-      nmap('<leader>fm', tb.builtin, { desc = 'fnd meta' })
+      nmap("<leader>fp", tb.help_tags, { desc = "find help" })
+      nmap("<leader>fm", tb.builtin, { desc = "fnd meta" })
 
       function vim.getVisualSelection()
-	vim.cmd('noau normal! "vy"')
-	local text = vim.fn.getreg('v')
-	vim.fn.setreg('v', {})
+        vim.cmd 'noau normal! "vy"'
+        local text = vim.fn.getreg "v"
+        vim.fn.setreg("v", {})
 
-	text = string.gsub(text, "\n", "")
-	if #text > 0 then
-	  return text
-	else
-	  return ''
-	end
+        text = string.gsub(text, "\n", "")
+        if #text > 0 then
+          return text
+        else
+          return ""
+        end
       end
 
       -- TODO: cache your search content
       -- TODO: fzf_lua really matters?
 
-      nmap('<space>e', ':Telescope current_buffer_fuzzy_find<cr>', opts)
-      vmap('<space>e', function()
-	local text = vim.getVisualSelection()
-	tb.current_buffer_fuzzy_find({ default_text = text })
+      nmap("<leader>e", ":Telescope current_buffer_fuzzy_find<cr>", opts)
+      vmap("<leader>e", function()
+        local text = vim.getVisualSelection()
+        tb.current_buffer_fuzzy_find { default_text = text }
       end)
 
-      -- nnoremap('<space>fj', ':Telescope live_grep<cr>', opts)
-      vmap('<space>fj', function()
-	local text = vim.getVisualSelection()
-	tb.live_grep({ default_text = text })
-      end)
+      vmap("<leader>fk", function() tb.live_grep { default_text = vim.getVisualSelection() } end)
 
+      nmap("<leader><space>", tb.find_files, { desc = "fzf files" })
+      nmap("<leader>l", tb.live_grep, { desc = "fzf grep" })
     end,
   },
 
   {
-    'crispgm/telescope-heading.nvim',
-    ft = { 'markdown', 'tex', },
-  }
+    "crispgm/telescope-heading.nvim",
+    ft = { "markdown", "tex" },
+  },
 }
