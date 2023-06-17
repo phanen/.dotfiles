2cd56d52f59b1575a5d616e48721b1673304fcd0
phanium <nxhsdu@qq.com>
2023-06-17 23:18:46 +0800

    feat: fake a config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,42 +1,121 @@
-local wezterm = require 'wezterm'
-wezterm.gui.enumerate_gpus()
+---@diagnostic disable: unused-local
+local wezterm = require("wezterm")
+
+wezterm.on("toggle-ligature", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.harfbuzz_features then
+        overrides.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+    else
+        overrides.harfbuzz_features = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
+local function modify_colorscheme(colorscheme)
+    local file_path = os.getenv("HOME") .. "/.config/wezterm/wezterm.lua"
+    local _f = assert(io.open(file_path, "r"))
+    local data = _f:read("*a")
+    _f:close()
+
+    local f = assert(io.open(file_path, "w"))
+    data = data:gsub(
+        'local color_scheme = "[^%%]-"',
+        ('local color_scheme = "%s"'):format(colorscheme)
+    )
+    f:write(data)
+    f:close()
+end
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+    local overrides = window:get_config_overrides() or {}
+    if name == "Nvim_Colorscheme" then
+        overrides.color_scheme = value
+        modify_colorscheme(value)
+    end
+    window:set_config_overrides(overrides)
+end)
+
+local color_scheme = "Rosé Pine Moon"
 
 return {
-  -- Smart tab bar [distraction-free mode]
-  hide_tab_bar_if_only_one_tab = true,
+    font = wezterm.font_with_fallback({
+        "FiraCode Nerd Font Mono",
+        "JetBrainsMono Nerd Font",
+        "Maple Mono SC NF", -- for Chinese
+        "Noto Color Emoji",
+        "JetBrains Mono",
 
-  -- Color scheme
-  -- https://wezfurlong.org/wezterm/config/appearance.html
-  --
-  -- Dracula
-  -- https://draculatheme.com
-  color_scheme = "Catppuccin Mocha",
+        -- icons
+        "Font Awesome 6 Pro Solid",
+        "Material Design Icons Desktop",
+        "MesloLGSDZ Nerd Font Mono",
+        "feather",
+    }),
+    font_size = 16.5,
 
+    color_scheme = color_scheme,
 
-  -- window_background_opacity = 0.90,
-  -- initial_cols = 150,
-  -- initial_rows = 40,
+    default_prog = { "/bin/bash", "-l", "-c", "tmux attach || tmux" },
 
-  -- Font configuration
-  -- https://wezfurlong.org/wezterm/config/fonts.html
-  font = wezterm.font('FiraCode Nerd Font Mono'),
-  font_size = 12.0,
+    window_padding = {
+        left = 10,
+        right = 2,
+        top = 0,
+        bottom = 0,
+    },
+    window_background_opacity = 0.95,
+    window_decorations = "RESIZE",
+    window_close_confirmation = "NeverPrompt",
 
-  -- Disable ligatures
-  -- https://wezfurlong.org/wezterm/config/font-shaping.html
-  -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+    enable_tab_bar = true,
+    use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+    tab_bar_at_bottom = false,
 
-  -- leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 },
-  -- keys = {
-  --   {
-  --     key = '|',
-  --     mods = 'LEADER|SHIFT',
-  --     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  --   },
-  --   {
-  --     key = '-',
-  --     mods = 'LEADER',
-  --     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  --   },
-  -- },
+    cursor_blink_ease_in = "Constant",
+    cursor_blink_ease_out = "Constant",
+
+    hyperlink_rules = {
+        -- Linkify things that look like URLs
+        -- This is actually the default if you don't specify any hyperlink_rules
+        {
+            regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
+            format = "$0",
+        },
+        -- match the URL with a PORT
+        -- such 'http://localhost:3000/index.html'
+        {
+            regex = "\\b\\w+://(?:[\\w.-]+):\\d+\\S*\\b",
+            format = "$0",
+        },
+        -- linkify email addresses
+        {
+            regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+            format = "mailto:$0",
+        },
+        -- file:// URI
+        {
+            regex = "\\bfile://\\S*\\b",
+            format = "$0",
+        },
+    },
+
+    keys = {
+        {
+            key = "Enter",
+            mods = "SHIFT",
+            action = wezterm.action.SendString("\x1b[13;2u"),
+        },
+        { key = "Tab", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
+        {
+            key = "Tab",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action({ ActivateTabRelative = -1 }),
+        },
+        {
+            key = "E",
+            mods = "CTRL",
+            action = wezterm.action.EmitEvent("toggle-ligature"),
+        },
+    },
 }
