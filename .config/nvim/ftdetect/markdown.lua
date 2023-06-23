2b7289e125163d9c90b4f5b927b6bf189b7178c5
phanium <nxhsdu@qq.com>
2023-06-23 16:53:02 +0800

    chore: update

--- .config/nvim/ftdetect/markdown.lua
+++ .config/nvim/ftdetect/markdown.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("x", "a", "<Plug>(nvim-surround-visual)j")
-vim.keymap.set("x", "i", "<Plug>(nvim-surround-visual)*")
+-- vim.keymap.set("x", "a", "<Plug>(nvim-surround-visual)j")
+-- vim.keymap.set("x", "i", "<Plug>(nvim-surround-visual)*")
---
+++ .config/nvim/lua/after/ftplugin/markdown.vim
@@ -0,0 +1,1 @@
+" https://vi.stackexchange.com/questions/13065/how-to-decrease-markdown-header-level-in-visual-mode-without-a-plugin
