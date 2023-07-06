a0c2ca0e77201705217283e3a2eb733c1c8a37ca
phanium <nxhsdu@qq.com>
2023-07-06 12:15:59 +0800

    feat: migrate to flash (from leap)

--- .config/nvim/lua/plugins/move.lua
+++ .config/nvim/lua/plugins/move.lua
@@ -6,28 +6,26 @@ local api, fn = vim.api, vim.fn
 local function falsy(item)
   if not item then return true end
   local item_type = type(item)
-  if item_type == 'boolean' then return not item end
-  if item_type == 'string' then return item == '' end
-  if item_type == 'number' then return item <= 0 end
-  if item_type == 'table' then return vim.tbl_isempty(item) end
+  if item_type == "boolean" then return not item end
+  if item_type == "string" then return item == "" end
+  if item_type == "number" then return item <= 0 end
+  if item_type == "table" then return vim.tbl_isempty(item) end
   return item ~= nil
 end
 
 local function leap_keys()
-  require('leap').leap({
-    target_windows = vim.tbl_filter(
-      function(win) return falsy(fn.win_gettype(win)) end,
-      api.nvim_tabpage_list_wins(0)
-    ),
-  })
+  require("leap").leap {
+    target_windows = vim.tbl_filter(function(win) return falsy(fn.win_gettype(win)) end, api.nvim_tabpage_list_wins(0)),
+  }
 end
 
 return {
   {
-    'ggandor/leap.nvim',
-    keys = { { 's', leap_keys, mode = 'n' } },
-    opts = { equivalence_classes = { ' \t\r\n', '([{', ')]}', '`"\'' } },
-    dependencies = 'tpope/vim-repeat',
+    "ggandor/leap.nvim",
+    cond = false,
+    keys = { { "s", leap_keys, mode = "n" } },
+    opts = { equivalence_classes = { " \t\r\n", "([{", ")]}", "`\"'" } },
+    dependencies = "tpope/vim-repeat",
   },
 
   -- {
@@ -42,5 +40,58 @@ return {
     dependencies = { "ggandor/leap.nvim" },
     config = true,
     lazy = false,
-  }
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    -- cond = false,
+    -- disable f F t T
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+        },
+      },
+    },
+    keys = {
+      {
+        "s",
+        mode = { "n", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "f",
+        mode = { "x" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      -- {
+      --   "S",
+      --   mode = { "n", "o" },
+      --   function() require("flash").treesitter() end,
+      --   desc = "Flash Treesitter",
+      -- },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+      -- {
+      --   "R",
+      --   mode = { "o", "x" },
+      --   function() require("flash").treesitter_search() end,
+      --   desc = "Flash Treesitter Search",
+      -- },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
 }
