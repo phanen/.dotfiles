1243ebe1358949da6eebf2affdbd12b9f75db903
phanium <nxhsdu@qq.com>
2023-07-11 01:16:15 +0800

    chore: update

--- .config/nvim/lua/plugins/move.lua
+++ .config/nvim/lua/plugins/move.lua
@@ -59,42 +59,18 @@ return {
       },
     },
     keys = {
+      { "s", mode = { "n", "o" }, function() require("flash").jump() end,       desc = "Flash", },
+      { "f", mode = { "x" },      function() require("flash").jump() end,       desc = "Flash", },
+      { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
+      { "r", mode = "o",          function() require("flash").remote() end,     desc = "Remote Flash", },
       {
-        "s",
-        mode = { "n", "o" },
-        function() require("flash").jump() end,
-        desc = "Flash",
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Flash Treesitter Search",
       },
-      {
-        "S",
-        mode = { "x" },
-        function() require("flash").jump() end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function() require("flash").remote() end,
-        desc = "Remote Flash",
-      },
-      -- {
-      --   "R",
-      --   mode = { "o", "x" },
-      --   function() require("flash").treesitter_search() end,
-      --   desc = "Flash Treesitter Search",
-      -- },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function() require("flash").toggle() end,
-        desc = "Toggle Flash Search",
-      },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search", },
     },
   },
 }
