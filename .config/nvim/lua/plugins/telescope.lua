56f3ff6ae602369b6246331d6a9b5cc44a9aae01
phanium <nxhsdu@qq.com>
2023-07-11 17:20:12 +0800

    feat: get selected prompt text

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,6 +31,7 @@ local curbuf_fzf = function()
   return tb.current_buffer_fuzzy_find { previewer = false, default_text = getVisualSelection() }
 end
 local live_grep = function() return tb.live_grep { default_text = getVisualSelection() } end
+local help_tags = function() return tb.help_tags { default_text = getVisualSelection() } end
 
 return {
   {
@@ -44,22 +45,27 @@ return {
 
     cmd = "Telescope",
     keys = {
-      { "<leader>ft", toggle_cache_mode,  mode = { "n", "x" }, desc = "pick files" },
-      { "<c-l>",      tb.find_files,      mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>l",  live_grep,          mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>/",  curbuf_fzf,         mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>fb", tb.buffers,         mode = { "n", "x" }, desc = "pick files" },
-      { "<ctrl>b",    tb.buffers,         mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>fo", tb.oldfiles,        mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>;",  tb.command_history, mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>fd", tb.diagnostics,     mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>fh", tb.help_tags,       mode = { "n", "x" }, desc = "pick files" },
-      { "<leader>fm", tb.builtin,         mode = { "n", "x" }, desc = "pick files" },
+      { "<leader>ft", toggle_cache_mode,       mode = { "n", "x" }, },
+      { "<leader>m",  tb.builtin,              mode = { "n", "x" }, },
+      { "<c-l>",      tb.find_files,           mode = { "n", "x" }, },
+      { "<c-b>",      tb.buffers,              mode = { "n", "x" }, },
+      { "<leader>j",  tb.oldfiles,             mode = { "n", "x" }, },
+      { "<leader>h",  help_tags,               mode = { "n", "x" }, },
+      { "<leader>l",  live_grep,               mode = { "n", "x" }, },
+      { "<leader>/",  curbuf_fzf,              mode = { "n", "x" }, },
+      { "<leader>;",  tb.command_history,      mode = { "n", "x" }, },
+      { "<leader>fo", tb.oldfiles,             mode = { "n", "x" }, },
+      { "<leader>fd", tb.diagnostics,          mode = { "n", "x" }, },
+      { "<leader>fh", tb.help_tags,            mode = { "n", "x" }, },
+      { "<leader>fg", tb.grep_string,          mode = { "n", "x" }, },
+      { "<leader>fs", tb.lsp_document_symbols, mode = { "n", "x" }, },
     },
     config = function()
       local tl = require "telescope"
       local ta = require "telescope.actions"
       local tag = require "telescope.actions.generate"
+      local tas = require "telescope.actions.state"
+
 
       tl.setup {
         defaults = {
@@ -70,13 +76,24 @@ return {
             i = {
               ["<c-u>"] = false,
               ["<c-d>"] = false,
+              ["<c-v>"] = false,
               ["<esc>"] = ta.close,
-              ["<a-/>"] = tag.which_key {
-                name_width = 20,           -- typically leads to smaller floats
-                max_height = 0.5,          -- increase potential maximum height
-                separator = " > ",         -- change sep between mode, keybind, and name
-                close_with_action = false, -- do not close float on action
-              },
+              ["<c-j>"] = ta.move_selection_next,
+              ["<c-k>"] = ta.move_selection_previous,
+              ["<c-g>"] = ta.move_to_bottom,
+              ["<c-l>"] = function(prompt_bufnr)
+                local entry = tas.get_selected_entry()
+                local prompt_text = entry.text or entry[1]
+                vim.fn.system("echo -n " .. prompt_text .. "| xsel -ib")
+                -- vim.api.nvim_paste(prompt_text, true, 1)
+                --   vim.api.nvim_get_current_buf()
+                ta.close(prompt_bufnr)
+                return true -- otherwise, cannot close?
+              end,
+
+              -- require('telescope.actions.state').get_current_picker(vim.api.nvim_buf_get_number(0))
+              -- https://stackoverflow.com/questions/74091577/how-to-get-prompt-value-in-telescope-vim
+              -- https://www.reddit.com/r/neovim/comments/11puvr6/getting_custom_telescope_live_grep_to_selectjump/
             },
           },
         },
