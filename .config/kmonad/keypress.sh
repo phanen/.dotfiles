8da6c4ec452da1fb801ca3630cabb0d080af319d
phanium <nxhsdu@qq.com>
2023-07-12 15:46:25 +0800

    feat: normal and visual mode

--- .config/kmonad/keypress.sh
+++ .config/kmonad/keypress.sh
@@ -4,10 +4,10 @@
 hookid=${1:-0}
 case $1 in
   qwe       ) hookid=0;;
-  rdl       ) hookid=1;;
-  cmk       ) hookid=2;;
-  cps       ) hookid=3;;
-  num       ) hookid=4;;
+  nor       ) hookid=1;;
+  vis       ) hookid=2;;
+  cmk       ) hookid=3;;
+  sym       ) hookid=4;;
 esac
 
 polybar-msg action kmonad hook ${hookid}
