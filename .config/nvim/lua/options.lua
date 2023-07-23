6eb1509836191bde55fca579fc75a6ff161caba7
phanium <nxhsdu@qq.com>
2023-07-23 15:32:11 +0800

    feat: lua to vim, vim to lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -8,7 +8,8 @@ o.number = true
 o.relativenumber = true
 
 -- https://stackoverflow.com/questions/2574027/automatically-go-to-next-line-in-vim--TODO
-vim.cmd [[set whichwrap+=h,l]]
+-- vim.cmd [[set whichwrap+=h,l]]
+o.whichwrap = o.whichwrap .. ",h,l"
 
 -- o.list = true
 -- o.listchars = (o.listchars .. 'eol:↳')
@@ -34,61 +35,6 @@ wo.signcolumn = 'yes'
 wo.conceallevel = 2
 
 o.termguicolors = true
-
--- o.completeopt = 'menuone,noselect,noinsert'
-
--- dump result of Ex
-vim.cmd [[
-	function! s:Capture(bang, cmd)
-	  let message = execute(a:cmd)
-
-	  if a:bang
-	    new
-	    setlocal buftype=nofile bufhidden=hide noswapfile
-	  endif
-
-	  call append('.', split(message, '\r\?\n'))
-	  redraw!
-	endfunction
-	command! -bang -nargs=+ -complete=command Capture call <SID>Capture("<bang>" == '!', <q-args>)
-
-	function! Redir(cmd, rng, start, end)
-		for win in range(1, winnr('$'))
-			if getwinvar(win, 'scratch')
-				execute win . 'windo close'
-			endif
-		endfor
-		if a:cmd =~ '^!'
-			let cmd = a:cmd =~' %'
-				\ ? matchstr(substitute(a:cmd, ' %', ' ' . shellescape(escape(expand('%:p'), '\')), ''), '^!\zs.*')
-				\ : matchstr(a:cmd, '^!\zs.*')
-			if a:rng == 0
-				let output = systemlist(cmd)
-			else
-				let joined_lines = join(getline(a:start, a:end), '\n')
-				let cleaned_lines = substitute(shellescape(joined_lines), "'\\\\''", "\\\\'", 'g')
-				let output = systemlist(cmd . " <<< $" . cleaned_lines)
-			endif
-		else
-			redir => output
-			execute a:cmd
-			redir END
-			let output = split(output, "\n")
-		endif
-		vnew
-		let w:scratch = 1
-		setlocal buftype=nofile bufhidden=wipe nobuflisted noswapfile
-		call setline(1, output)
-	endfunction
-
-	" This command definition includes -bar, so that it is possible to "chain" Vim commands.
-	" Side effect: double quotes can't be used in external commands
-	command! -nargs=1 -complete=command -bar -range Redir silent call Redir(<q-args>, <range>, <line1>, <line2>)
-
-	" This command definition doesn't include -bar, so that it is possible to use double quotes in external commands.
-	" Side effect: Vim commands can't be "chained".
-	command! -nargs=1 -complete=command -range Redir silent call Redir(<q-args>, <range>, <line1>, <line2>)
-]]
 
 --Set completeopt to have a better completion experience
 -- :help completeopt
@@ -116,7 +62,6 @@ vim.opt.wildignore:append({ ".aux", ".out", ".toc" }) -- LaTeX
 vim.opt.wildignore:append({ ".o", ".obj", ".dll", ".exe", ".so", ".a", ".lib", ".pyc", ".pyo", ".pyd", ".swp", ".swo",
 	".class", ".DS_Store", ".git", ".hg", ".orig" })
 
-
--- avoid escape become alt
-o.nottimeoutlen = true
--- o.ttimeoutlen = 10
+-- avoid sticky escape as alt
+o.ttimeout = false
+o.ttimeoutlen = 10
