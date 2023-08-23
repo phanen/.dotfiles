3309af8031feb52b64ac951b6c20acc9b1544088
phancb <nxhsdu@qq.com>
2023-08-23 09:16:59 +0800

    feat(search): add browse.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,14 +1,39 @@
+local function getVisualText()
+  local reg_bak = vim.fn.getreg "v"
+  vim.fn.setreg("v", {})
+  vim.cmd [[noau normal! "vy\<esc\>]]
+  local sel_text = vim.fn.getreg "v"
+  vim.fn.setreg("v", reg_bak)
+  return #sel_text == 0 and "" or string.gsub(sel_text, "\n", "")
+end
+
 return {
-  -- toggle fold
-  -- {
-  --   'jghauser/fold-cycle.nvim',
-  --   config = true,
-  --   keys = {
-  --     { '<leader>of', function() require('fold-cycle').open() end, desc = 'fold-cycle: toggle' },
-  --   },
-  -- },
 
   -- diff arbitrary blocks of text with each other
   { "AndrewRadev/linediff.vim", cmd = "Linediff" },
   { "jspringyc/vim-word", cmd = { "WordCount", "WordCountLine" } },
+
+  {
+    "lalitmee/browse.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      {
+        mode = { "n", "x" },
+        "<leader>S",
+        function() require("browse").open_bookmarks() end,
+      },
+      {
+        mode = { "n", "x" },
+        "<leader>B",
+        function() require("browse").browse() end,
+      },
+    },
+
+    opts = {
+      bookmarks = {
+        ["github_code_search"] = "https://github.com/search?q=%s&type=code",
+        ["github_repo_search"] = "https://github.com/search?q=%s&type=repositories",
+      },
+    },
+  },
 }
