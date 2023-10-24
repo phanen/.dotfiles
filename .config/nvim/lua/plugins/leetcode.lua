96943870a953ddba69ee703da5b6aa32e46b0303
phancb <nxhsdu@qq.com>
2023-10-24 14:15:46 +0800

    chore: update

---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,20 @@
+local leet_arg = "leetcode.nvim"
+
+return {
+  "kawre/leetcode.nvim",
+  lazy = leet_arg ~= vim.fn.argv()[1],
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    arg = leet_arg,
+  },
+}
