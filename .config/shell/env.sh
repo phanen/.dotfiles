58ac27e4bb5f5670a364f72749f46c96fe2debab
phancb <nxhsdu@qq.com>
2023-12-17 23:06:50 +0800

    chore: update

--- .config/shell/env.sh
+++ .config/shell/env.sh
@@ -77,33 +77,8 @@ export NVBOARD_HOME=$HOME/ysyx-workbench/nvboard
 # remove all but the last identical command, and commands that start with a space
 export HISTCONTROL="erasedups:ignorespace"
 
-# 启动 wayland 桌面前设置一些环境变量
-function set_wayland_env
-{
-  # 解决QT程序缩放问题
-  export QT_AUTO_SCREEN_SCALE_FACTOR=1
-  # QT使用wayland和gtk
-  export QT_QPA_PLATFORM="wayland;xcb"
-  export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-  # 使用qt5ct软件配置QT程序外观
-  export QT_QPA_PLATFORMTHEME=qt5ct
-
-  # 一些游戏使用wayland
-  export SDL_VIDEODRIVER=wayland
-  # 解决java程序启动黑屏错误
-  export _JAVA_AWT_WM_NONEREPARENTING=1
-  # GTK后端为 wayland和x11,优先wayland
-  export GDK_BACKEND="wayland,x11"
-}
-
-# 命令行输入这个命令启动hyprland,可以自定义
-function start_hyprland
-{
-  set_wayland_env
-
-  export XDG_SESSION_TYPE=wayland
-  export XDG_SESSION_DESKTOP=Hyprland
-  export XDG_CURRENT_DESKTOP=Hyprland
-  # 启动 Hyprland程序
-  Hyprland
-}
+# actually this file is a profile
+case  $(tty) in
+  *tty*) setfont ter-d28b;;
+  *) ;;
+esac
