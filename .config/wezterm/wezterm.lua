1a19f412ac4367e4752de44cc55e892cef426b88
phancb <nxhsdu@qq.com>
2023-12-17 23:05:15 +0800

    feat: upd wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,121 +1,70 @@
----@diagnostic disable: unused-local
 local wezterm = require("wezterm")
+local act = wezterm.action
 
-wezterm.on("toggle-ligature", function(window, pane)
-    local overrides = window:get_config_overrides() or {}
-    if not overrides.harfbuzz_features then
-        overrides.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
-    else
-        overrides.harfbuzz_features = nil
-    end
-    window:set_config_overrides(overrides)
-end)
-
-local function modify_colorscheme(colorscheme)
-    local file_path = os.getenv("HOME") .. "/.config/wezterm/wezterm.lua"
-    local _f = assert(io.open(file_path, "r"))
-    local data = _f:read("*a")
-    _f:close()
-
-    local f = assert(io.open(file_path, "w"))
-    data = data:gsub(
-        'local color_scheme = "[^%%]-"',
-        ('local color_scheme = "%s"'):format(colorscheme)
-    )
-    f:write(data)
-    f:close()
+-- this will auto re-run when dbus notify it
+local function get_scheme()
+	local appearance = wezterm.gui.get_appearance()
+	if appearance:find("Light") then
+		-- return "dayfox"
+		return "catppuccin-latte"
+	end
+	return "Dracula (Official)"
 end
 
-wezterm.on("user-var-changed", function(window, pane, name, value)
-    local overrides = window:get_config_overrides() or {}
-    if name == "Nvim_Colorscheme" then
-        overrides.color_scheme = value
-        modify_colorscheme(value)
-    end
-    window:set_config_overrides(overrides)
-end)
+-- note: may need restart to make some changes bind work correctly
+local M = {
+	font = wezterm.font_with_fallback({
+		"CaskaydiaCove Nerd Font",
+		"Noto Sans CJK SC",
+	}),
+	font_size = 13.5,
+	color_scheme = get_scheme(),
+	default_prog = { "fish" },
 
-local color_scheme = "Rosé Pine Moon"
+	use_ime = true,
+	check_for_updates = false,
+	-- FIXME: not work
+	automatically_reload_config = false,
 
-return {
-    font = wezterm.font_with_fallback({
-        "FiraCode Nerd Font Mono",
-        "JetBrainsMono Nerd Font",
-        "Maple Mono SC NF", -- for Chinese
-        "Noto Color Emoji",
-        "JetBrains Mono",
+	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+	window_background_opacity = 1,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
 
-        -- icons
-        "Font Awesome 6 Pro Solid",
-        "Material Design Icons Desktop",
-        "MesloLGSDZ Nerd Font Mono",
-        "feather",
-    }),
-    font_size = 16.5,
-
-    color_scheme = color_scheme,
-
-    default_prog = { "/bin/bash", "-l", "-c", "tmux attach || tmux" },
-
-    window_padding = {
-        left = 10,
-        right = 2,
-        top = 0,
-        bottom = 0,
-    },
-    window_background_opacity = 0.95,
-    window_decorations = "RESIZE",
-    window_close_confirmation = "NeverPrompt",
-
-    enable_tab_bar = true,
-    use_fancy_tab_bar = true,
-    hide_tab_bar_if_only_one_tab = true,
-    tab_bar_at_bottom = false,
-
-    cursor_blink_ease_in = "Constant",
-    cursor_blink_ease_out = "Constant",
-
-    hyperlink_rules = {
-        -- Linkify things that look like URLs
-        -- This is actually the default if you don't specify any hyperlink_rules
-        {
-            regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
-            format = "$0",
-        },
-        -- match the URL with a PORT
-        -- such 'http://localhost:3000/index.html'
-        {
-            regex = "\\b\\w+://(?:[\\w.-]+):\\d+\\S*\\b",
-            format = "$0",
-        },
-        -- linkify email addresses
-        {
-            regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-            format = "mailto:$0",
-        },
-        -- file:// URI
-        {
-            regex = "\\bfile://\\S*\\b",
-            format = "$0",
-        },
-    },
-
-    keys = {
-        {
-            key = "Enter",
-            mods = "SHIFT",
-            action = wezterm.action.SendString("\x1b[13;2u"),
-        },
-        { key = "Tab", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
-        {
-            key = "Tab",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ ActivateTabRelative = -1 }),
-        },
-        {
-            key = "E",
-            mods = "CTRL",
-            action = wezterm.action.EmitEvent("toggle-ligature"),
-        },
-    },
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = false,
 }
+
+-- wezterm show-keys --lua  &| less
+M.key_tables = {
+	-- PERF: need confirm-mode
+	-- search_mode = {
+	-- },
+	-- no copy mode?
+}
+
+M.keys = {
+	{ mods = "SHIFT|CTRL", key = "R",          action = act.ReloadConfiguration },
+
+	-- modes
+	{ mods = "SHIFT",      key = "Space",      action = act.ActivateCopyMode },
+	{ mods = "SHIFT|CTRL", key = "phys:Space", action = act.QuickSelect },
+	{ mods = "SHIFT|CTRL", key = "p",          action = act.ActivateCommandPalette },
+	{ mods = "SHIFT|CTRL", key = "f",          action = act.Search("CurrentSelectionOrEmptyString") },
+	-- TODO: wtf
+	{ mods = "SHIFT|CTRL", key = "z",          action = act.TogglePaneZoomState },
+	{ mods = "SHIFT|CTRL", key = "k",          action = act.ClearScrollback("ScrollbackOnly") },
+	{ mods = "SUPER",      key = "l",          action = act.ActivateTabRelative(1) },
+	{ mods = "SUPER",      key = "h",          action = act.ActivateTabRelative(-1) },
+	{ mods = "SUPER",      key = "u",          action = act.SpawnTab("CurrentPaneDomain") },
+	{ mods = "SHIFT|CTRL", key = "w",          action = act.CloseCurrentTab({ confirm = false }) },
+	-- clipboard
+	-- PERF: copy-or-interrupt
+	-- { mods = "CTRL",       key = "c",          action = act.CopyTo("Clipboard") },
+	{ mods = "SHIFT|CTRL", key = "c",          action = act.CopyTo("Clipboard") },
+	{ mods = "CTRL",       key = "v",          action = act.PasteFrom("Clipboard") },
+}
+
+return M
