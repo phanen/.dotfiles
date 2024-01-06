af19b608ccdc644b12146b3bbda84b1496b4c8b7
phanium <nxhsdu@qq.com>
2024-01-06 23:30:36 +0800

    fix: tty

--- .bin/sh2fish.sh
+++ .bin/sh2fish.sh
@@ -3,4 +3,4 @@
 SHELLRC=~/.shellrc
 FISH_COMMON=~/.config/fish/sh-common.fish
 cat ${SHELLRC} | sed 's/^.*\]\] && \./cat/e' | sed '/^[[:blank:]]*#/d;s/#.*//' | babelfish > ${FISH_COMMON}
-[[ $(tty) != /dev/tty* ]] && dunstify sh2fish || exit 0
+[[ $(tty) != /dev/tty* && -z $SSH_TTY ]] && dunstify sh2fish || exit 0
