e48c21197eb9106fabb350c0df0d8fdc01b87d87
phanium <nxhsdu@qq.com>
2024-03-18 13:48:57 +0800

    refactor: fzf-lua again

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,28 +1,58 @@
-local opts_cb = function(k)
+local opts_fn = ...
+local overlay = ...
+local notes_actions = ...
+
+local fl = setmetatable({}, {
+  __index = function(_, k)
+    return function()
+      local opts, ropts, key, cmd
+      key, opts, cmd = unpack(overlay[k])
+      ropts = opts_fn(k)
+      opts = vim.tbl_deep_extend('force', opts, ropts or {})
+      if cmd then
+        require('fzf-lua').fzf_exec(cmd, opts)
+      else
+        require('fzf-lua')[key](opts)
+      end
+    end
+  end,
+})
+
+opts_fn = function(k)
   local text = table.concat(util.getregion())
-  local opts = {}
   if k:match('grep') then
-    opts.search = text
-    return opts
+    return { search = text }
+  else
+    return { fzf_opts = { ['--query'] = text ~= '' and text or nil } }
   end
-  opts.fzf_opts = { ['--query'] = text ~= '' and text or nil }
-  return opts
 end
 
-local notes_actions = {
+notes_actions = {
+  ['ctrl-g'] = {
+    function(_, opts)
+      local o = opts.__call_opts
+      if opts.show_daily_only then
+        o.cmd = 'fd --color=never --type f --hidden --follow --exclude .git'
+      else
+        o.cmd = 'fd "[0-9][0-9]-[0-9][0-9]*"  --type f'
+      end
+      o.show_daily_only = not opts.show_daily_only
+      opts.__call_fn(o)
+    end,
+  },
   ['ctrl-n'] = function(_, opts)
     local query = require('fzf-lua').get_last_query()
-    if query == '' then
+    if not query or query == '' then
       query = os.date('%m-%d')
     end
     local path = vim.fn.expand(fmt('%s/%s.md', opts.cwd, query))
     if not vim.uv.fs_stat(path) then
       local file = io.open(path, 'a')
-      if file == nil then
-        vim.notify('fzf-lua: fail to create file ' .. path)
+      if not file then
+        vim.notify(('fail to create file %s'):format(path))
         return
       end
-      vim.notify(fmt('fzf-lua: %s has been created', path))
+      vim.notify(('%s has been created'):format(path))
       file:close()
     end
     vim.cmd.e(path)
@@ -30,48 +60,26 @@ local notes_actions = {
   ['ctrl-x'] = {
     fn = function(selected, opts)
       -- TODO: multi?
-      local path = vim.fn.expand(fmt('%s/%s', opts.cwd, selected[1]))
-      -- TODO: vim ui?
-      local confirm = vim.fn.confirm(fmt('Delete %s', path), '&y\n&n')
-      if confirm == 1 then
-        if not vim.uv.fs_stat(path) then
-          return nil
-        end
-        vim.uv.fs_unlink(path)
-        -- vim.notify(path .. ' has been deleted')
-      end
+      local cwd = opts.cwd or vim.fn.getcwd()
+      local path = vim.fn.expand(('%s/%s'):format(cwd, selected[1]))
+      local _fn, _opts = opts.__call_fn, opts.__call_opts
+      require('fzf-lua').fzf_exec({ 'YES', 'NO' }, {
+        prompt = ('Delete %s'):format(path),
+        actions = {
+          ['default'] = function(sel)
+            if sel[1] == 'YES' and vim.uv.fs_stat(path) then
+              vim.uv.fs_unlink(path)
+              vim.notify(('%s has been deleted'):format(path))
+            end
+            _fn(_opts)
+          end,
+        },
+      })
     end,
-    reload = true,
-  },
-  ['ctrl-r'] = {
-    fn = function(selected, opts)
-      local filename = selected[1]
-      local prefix = vim.fn.expand(opts.cwd)
-      local path = fmt('%s/%s', prefix, filename)
-      -- TODO: ignore ext name
-      -- xx.xx, x.xx.xx, .xx, x.
-      vim.split(filename, '.', { plain = true })
-      local newname = vim.fn.input('New name: ', filename)
-      local newpath = fmt('%s/%s', prefix, newname)
-      if vim.trim(newpath) == '' then
-        vim.notify('fzf-lua: empty name')
-      end
-      vim.uv.fs_rename(path, newpath)
-      vim.notify(fmt('%s has been renamed to %s', path, newpath))
-      -- require('fzf-lua').resume()
-      -- TODO: vim ui
-      -- TODO: missing cursor
-      -- vim.ui.input({ prompt = 'New name:', default = filename }, function(newname)
-      --   if newname == nil then
-      --     return
-      --   end
-      -- end)
-    end,
-    reload = true,
   },
 }
 
-local overlay = {
+overlay = setmetatable({
   find_dots = { 'files', { cwd = '~' } },
   live_grep_notes = { 'live_grep_native', { cwd = '~' } },
   todo_comment = { 'grep', { search = 'TODO|HACK|PERF|NOTE|FIX', no_esc = true } },
@@ -84,123 +92,79 @@ local overlay = {
     {
       cwd = '~/notes',
       actions = notes_actions,
+      fzf_opts = {
+        ['--history'] = vim.fn.stdpath 'state' .. '/fzf_notes_history',
+      },
       file_icons = false,
       git_icons = false,
     },
   },
-  find_daily = {
-    'files',
+  zoxide = {
+    'fzf_exec',
     {
-      cmd = 'fd "[0-9][0-9]-[0-9][0-9]*"  --type f',
-      cwd = '~/notes',
-      actions = notes_actions,
-      file_icons = false,
-      git_icons = false,
+      prompt = 'zoxide>',
+      actions = {
+        ['default'] = function(selected)
+          local path = selected[1]:match('/.+')
+          vim.api.nvim_set_current_dir(path)
+        end,
+      },
     },
+    'zoxide query -ls',
   },
-}
-
----@module 'fzf-lua'
-local fl = setmetatable({}, {
-  __index = function(_, k)
-    return function()
-      local opts = opts_cb(k)
-      local picker = k
-      local preset = overlay[k]
-      if preset then
-        local old = opts
-        picker, opts = unpack(preset)
-        opts = vim.tbl_deep_extend('force', old, opts or {})
-        if picker:match('lsp_') then
-          opts.jump_to_single_result = true
-        end
-      end
-      require('fzf-lua')[picker](opts)
+}, { -- other static opts lazy to write
+  __index = function(t, k)
+    local opts = {}
+    if k:match('lsp') then
+      opts.jump_to_single_result = true
     end
+    local v = { k, opts }
+    t[k] = v
+    return v
   end,
 })
-
--- TODO: extend dir history to fzf-lua ver
--- since zoxide can not be hack into module, we can
-local zoxide = function(opts)
-  opts = opts or {}
-  opts.prompt = 'zoxide> '
-  local fzf_q = table.concat(util.getregion())
-  opts.fzf_opts = {
-    ['--query'] = fzf_q ~= '' and fzf_q or nil, -- fix injection
-    ['--history'] = vim.fn.stdpath 'state' .. '/telescope_history',
-  }
-  opts.actions = {
-    ['default'] = function(selected)
-      local path = selected[1]:match('/.+')
-      vim.api.nvim_set_current_dir(path)
-    end,
-  }
-  require('fzf-lua').fzf_exec('zoxide query -ls', opts)
-end
 
 return {
   {
     'junegunn/fzf.vim',
     cmd = { 'Files', 'RG', 'Rg' },
     dependencies = { 'junegunn/fzf' },
-    keys = {
-      { '<leader><c-l>', '<cmd>Files<cr>', mode = { 'n', 'x' } },
-      { '<leader><c-k>', '<cmd>Rg<cr>', mode = { 'n', 'x' } },
-    },
-  },
-  {
-    'phanen/fzf-lua-ext',
-    dependencies = { 'ibhagwan/fzf-lua' },
-    cond = false,
-    -- stylua: ignore
-    keys = {
-      { '<leader><c-f>', function() require('fzf-lua-ext').zoxide() end,       mode = { 'n', 'x' } },
-      { '<leader><c-j>', function() require('fzf-lua-ext').todo_comment() end, mode = { 'n', 'x' } },
-      { '<leader>e',     function() require('fzf-lua-ext').find_notes() end,   mode = { 'n', 'x' } },
-      { '<leader>fe',    function() require('fzf-lua-ext').find_daily() end,   mode = { 'n', 'x' } },
-    },
   },
   {
     'ibhagwan/fzf-lua',
     cmd = { 'FzfLua' },
     -- stylua: ignore
     keys = {
-      { '<c-b>',         fl.buffers,                    mode = { 'n', 'x' } },
-      { '<c-l>',         fl.files,                      mode = { 'n', 'x' } },
-      { '<c-n>',         fl.live_grep_native,           mode = { 'n', 'x' } },
-      { '<c-x><c-b>',    fl.complete_bline,             mode = 'i' },
-      { '<c-x><c-f>',    fl.complete_file,              mode = 'i' },
-      { '<c-x><c-p>',    fl.complete_path,              mode = 'i' },
-      { 'gd',            fl.lsp_definitions,            mode = { 'n', 'x' } },
-      { 'gh',            fl.lsp_code_actions,           mode = { 'n', 'x' } },
-      { 'gr',            fl.lsp_references,             mode = { 'n', 'x' } },
-      { '<leader><c-f>', zoxide,                        mode = { 'n', 'x' } },
-      { '<leader><c-j>', fl.todo_comment,               mode = { 'n', 'x' } },
-      { '<leader>e',     fl.find_notes,                 mode = { 'n', 'x' } },
-      { '<leader>fa',    fl.builtin,                    mode = { 'n', 'x' } },
-      { '<leader>fe',    fl.find_daily,                 mode = { 'n', 'x' } },
-      { '<leader>f;',    fl.command_history,            mode = { 'n', 'x' } },
-      { '<leader>fh',    fl.help_tags,                  mode = { 'n', 'x' } },
-      { '<leader>fj',    fl.live_grep_notes,            mode = { 'n', 'x' } },
-      { '<leader>fk',    fl.keymaps,                    mode = { 'n', 'x' } },
-      { '<leader>/',     fl.blines,                     mode = { 'n', 'x' } },
-      { '<leader> ',     fl.resume,                     mode = 'n' },
-      { '<leader>;',     fl.spell_suggest,              mode = { 'n', 'x' } },
-      { '<leader>fo',    fl.oldfiles,                   mode = { 'n', 'x' } },
-      { '<leader>fs',    fl.lsp_document_symbols,       mode = { 'n', 'x' } },
+      { '<c-b>',         fl.buffers,               mode = { 'n', 'x' } },
+      { '<c-l>',         fl.files,                 mode = { 'n', 'x' } },
+      { '<c-n>',         fl.live_grep_native,      mode = { 'n', 'x' } },
+      { '<c-x><c-b>',    fl.complete_bline,        mode = 'i' },
+      { '<c-x><c-f>',    fl.complete_file,         mode = 'i' },
+      { '<c-x><c-p>',    fl.complete_path,         mode = 'i' },
+      { 'gd',            fl.lsp_definitions,       mode = { 'n', 'x' } },
+      { 'gh',            fl.lsp_code_actions,      mode = { 'n', 'x' } },
+      { 'gr',            fl.lsp_references,        mode = { 'n', 'x' } },
+      { '<leader><c-f>', fl.zoxide,                mode = { 'n', 'x' } },
+      { '<leader><c-j>', fl.todo_comment,          mode = { 'n', 'x' } },
+      { '<leader>e',     fl.find_notes,            mode = { 'n', 'x' } },
+      { '<leader>fa',    fl.builtin,               mode = { 'n', 'x' } },
+      { '<leader>f;',    fl.command_history,       mode = { 'n', 'x' } },
+      { '<leader>fh',    fl.help_tags,             mode = { 'n', 'x' } },
+      { '<leader>fj',    fl.live_grep_notes,       mode = { 'n', 'x' } },
+      { '<leader>fk',    fl.keymaps,               mode = { 'n', 'x' } },
+      { '<leader>/',     fl.blines,                mode = { 'n', 'x' } },
+      { '<leader> ',     fl.resume,                mode = 'n' },
+      { '<leader>;',     fl.spell_suggest,         mode = { 'n', 'x' } },
+      -- { '<leader>fo',    fl.oldfiles,              mode = { 'n', 'x' } },
+      { '<leader>fs',    fl.lsp_document_symbols,  mode = { 'n', 'x' } },
       { '<leader>fw',    fl.lsp_workspace_symbols, mode = { 'n', 'x' } },
-      { '<leader>gd',    fl.lsp_typedefs,               mode = { 'n', 'x' } },
-      { '<leader>l',     fl.find_dots,                  mode = { 'n', 'x' } },
+      { '<leader>gd',    fl.lsp_typedefs,          mode = { 'n', 'x' } },
+      { '<leader>l',     fl.find_dots,             mode = { 'n', 'x' } },
     },
-    -- config = function(_, opts)
-    --   require('fzf-lua').setup(opts or {})
-    -- end,
     opts = {
       previewers = {
         builtin = {
           extensions = {
-            -- TODO: neovim terminal only supports `viu` block output
             ['png'] = { 'viu', '-b' },
             ['jpg'] = { 'ueberzug' },
           },
@@ -241,19 +205,41 @@ return {
           ['ctrl-s'] = function(...)
             require('fzf-lua').actions.file_edit_or_qf(...)
           end,
-          ['ctrl-l'] = {
+          ['ctrl-y'] = {
             fn = function(selected)
-              -- TODO: pattern match it
               vim.fn.setreg('+', selected[1]:sub(7))
             end,
             reload = true,
           },
-          -- FIXME: unkown
-          ['ctrl-o'] = {
-            fn = function(...)
-              require('fzf-lua').actions.file_edit_or_qf(...)
+          ['ctrl-r'] = { -- TODO: no cursor
+            fn = function(selected, opts)
+              local file = require('fzf-lua').path.entry_to_file(selected[1], opts)
+              local oldpath = file.path
+              local oldname = vim.fs.basename(oldpath)
+              local newname = vim.fn.input('New name: ', oldname)
+              newname = vim.trim(newname)
+              if newname == '' or newname == oldname then
+                return
+              end
+              local cwd = opts.cwd or vim.fn.getcwd()
+              local newpath = ('%s/%s'):format(cwd, newname)
+              vim.uv.fs_rename(oldpath, newpath)
+              vim.notify(
+                ('%s has been renamed to %s'):format(oldpath, newpath),
+                vim.log.levels.INFO
+              )
             end,
             reload = true,
+          },
+          ['ctrl-o'] = { -- call back?
+            fn = function(selected, opts)
+              for _, sel in ipairs(selected) do
+                local file = require('fzf-lua').path.entry_to_file(sel, opts)
+                vim.schedule_wrap(function()
+                  vim.cmd.e(file.path)
+                end)
+              end
+            end,
           },
         },
       },
