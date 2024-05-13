c9101ce35b7202db5a6ae080d2de8bc9a44b3b09
phanium <nxhsdu@qq.com>
2024-05-14 00:45:22 +0800

    feat: upd swaywm and katana

--- .bin/xrate-refresh.sh
+++ .bin/xrate-refresh.sh
@@ -2,11 +2,11 @@
 
 # https://unix.stackexchange.com/questions/121858/how-can-i-set-repeat-rate-of-usb-keyboard-with-udev
 (
-	set -e
-    PID=$(pgrep bspwm | head -1)
-    eval "$(sed 's:\x0:\n:g' /proc/$PID/environ | rg 'DISPLAY=' | head -1)"
-    # TODO: use udev rule local to user?
-    eval "$(sed 's:\x0:\n:g' /proc/$PID/environ | rg 'USER=' | head -1)"
-    XAUTHORITY="/home/$USER/.Xauthority"
-    DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY xset r rate 140 100
+  set -e
+  PID=$(pgrep bspwm | head -1)
+  eval "$(sed 's:\x0:\n:g' /proc/$PID/environ | rg 'DISPLAY=' | head -1)"
+  # TODO: use udev rule local to user?
+  eval "$(sed 's:\x0:\n:g' /proc/$PID/environ | rg 'USER=' | head -1)"
+  XAUTHORITY="/home/$USER/.Xauthority"
+  DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY xset r rate 140 100
 ) &
