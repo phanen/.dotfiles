107f766c6474bb3e1901d79442f9659d5ca6949d
phanium <nxhsdu@qq.com>
2024-09-01 14:26:31 +0800

    chore: use upstream

--- .config/nvim/lua/mod/winbar/utils/init.lua
+++
@@ -1,9 +0,0 @@
----TODO: extend class (missing u.lib.stl lsp...)
----@class mod.winbar.utils: extend 'lib.stl'
-return setmetatable({}, {
-  __index = function(self, key)
-    local ok, local_util = pcall(require, 'mod.winbar.utils.' .. key)
-    self[key] = ok and local_util or u[key]
-    return self[key]
-  end,
-})
