64a8dcb0af4cb253a4480837af86a8928a1ee387
phanium <nxhsdu@qq.com>
2024-09-11 12:51:42 +0800

    tweak: repl

--- .config/nvim/lua/plugs.lua
+++ .config/nvim/lua/plugs.lua
@@ -53,26 +53,60 @@ return {
     config = true,
   },
 
-  -- TODO: vendor it
+  -- repl (maybe needless when we has a runner/sourcer over flie)
   {
-    'ghostbuster91/nvim-next',
+    'Vigemus/iron.nvim',
     cond = false,
-    -- treesitter don't lazy-load, so needless
-    -- keys = { ' dj', ' dk', ' <c-g>p', ' <c-g>n' },
+    lazy = false,
     config = function()
-      local next = require 'nvim-next'
-      local b = require 'nvim-next.builtins'
-      local i = require 'nvim-next.integrations'
-      next.setup {
-        default_mappings = { repeat_style = 'original' },
-        -- items = { b.f, b.t },
+      require('iron.core').setup {
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { 'zsh' },
+            },
+            python = {
+              command = { 'python3' }, -- or { "ipython", "--no-autoindent" }
+              format = require('iron.fts.common').bracketed_paste_python,
+            },
+            lua = {
+              command = { 'lua' },
+            },
+          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          repl_open_cmd = require('iron.view').bottom(40),
+        },
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          send_motion = '<space>sc',
+          visual_send = '<space>sc',
+          send_file = '<space>sf',
+          send_line = '<space>sl',
+          send_paragraph = '<space>sp',
+          send_until_cursor = '<space>su',
+          send_mark = '<space>sm',
+          mark_motion = '<space>mc',
+          mark_visual = '<space>mc',
+          remove_mark = '<space>md',
+          cr = '<space>s<cr>',
+          interrupt = '<space>s<space>',
+          exit = '<space>sq',
+          clear = '<space>cl',
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
       }
-      local diag = i.diagnostic()
-      local nqf = i.quickfix()
-      map.n(' dk', diag.goto_prev())
-      map.n(' dj', diag.goto_next())
-      map.n('<c-g>p', nqf.cprevious)
-      map.n('<c-g>n', nqf.cnext)
     end,
   },
 }
