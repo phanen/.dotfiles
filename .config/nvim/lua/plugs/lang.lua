3104b9ec4632bb3d8f7ab1cb43612a060a538ec0
phanium <nxhsdu@qq.com>
2024-11-18 17:45:49 +0800

    chore: cleanup

--- .config/nvim/lua/plugs/lang.lua
+++ .config/nvim/lua/plugs/lang.lua
@@ -2,11 +2,7 @@ local specs = {
   -- syntax
   { 'HiPhish/info.vim', cmd = 'Info' },
   { 'fladson/vim-kitty', ft = 'kitty' },
-  { 'chrisbra/csv.vim', ft = 'csv' },
-  { 'kmonad/kmonad-vim', ft = 'kbd' },
   { 'baskerville/vim-sxhkdrc', event = 'BufReadPre sxhkdrc' },
-  { 'NoahTheDuke/vim-just', ft = 'just' },
-  { 'Fymyte/rasi.vim', ft = 'rasi' },
 
   -- text process
   { 'hotoo/pangu.vim', cmd = 'Pangu', ft = 'markdown' },
