fc95f7c8838851ea6c05c639697cc08cc4b33925
phanium <nxhsdu@qq.com>
2024-11-23 14:43:58 +0800

    feat: document highlight and codelens

--- .config/nvim/lua/ulib/lsp.lua
+++ .config/nvim/lua/ulib/lsp.lua
@@ -22,45 +22,41 @@ Lsp.on = function(_)
     float = { border = 'none' },
   }
 
+  if true then return end
+
   -- https://github.com/lewis6991/dotfiles/blob/main/config%2Fnvim%2Flua%2Flewis6991%2Flsp.lua
+  local method = 'textDocument/documentHighlight'
+  local f = function(args)
+    -- if move_out_visual then
+    lsp.buf.clear_references()
+    -- end
+    local bufnr = args.buf --- @type integer
+    local win = api.nvim_get_current_win()
+    for _, client in ipairs(lsp.get_clients { bufnr = bufnr, method = method }) do
+      local enc = client.offset_encoding
+      local param = lsp.util.make_position_params(0, enc)
+      client:request(method, param, function(_, result, ctx)
+        if not result or win ~= api.nvim_get_current_win() then return end
+        lsp.util.buf_highlight_references(ctx.bufnr, result, client.offset_encoding)
+      end, bufnr)
+    end
+  end
+  u.aug.lsp_document_highlight = {
+    { 'FocusGained', 'WinEnter', 'BufEnter', 'CursorMoved' },
+    { callback = u.lodash.debounce(200, f) },
+    { 'FocusLost', 'WinLeave', 'BufLeave' },
+    { callback = lsp.buf.clear_references },
+  }
 
-  -- do -- textDocument/documentHighlight
-  --   local f = function(args)
-  --     -- if move_out_visual then
-  --     lsp.buf.clear_references()
-  --     -- end
-  --     local bufnr = args.buf --- @type integer
-  --     for _, client in ipairs(lsp.get_clients({ bufnr = bufnr })) do
-  --       if client.supports_method('textDocument/documentHighlight', { bufnr = bufnr }) then
-  --         local params = lsp.util.make_position_params()
-  --         client.request('textDocument/documentHighlight', params, nil, bufnr)
-  --       end
-  --       -- FIXME: when there's multiple server...
-  --       break
-  --     end
-  --   end
-  --
-  --   api.nvim_create_autocmd(
-  --     -- { 'FocusGained', 'WinEnter', 'BufEnter', 'CursorMoved', 'CursorHold', 'CursorHoldI' },
-  --     { 'FocusGained', 'WinEnter', 'BufEnter', 'CursorHold', 'CursorHoldI' },
-  --     { callback = f }
-  --     -- { callback = u.lodash.debounce(200, f) }
-  --   )
-  --
-  --   api.nvim_create_autocmd(
-  --     { 'FocusLost', 'WinLeave', 'BufLeave' },
-  --     { callback = lsp.buf.clear_references }
-  --   )
-  -- end
-
-  -- -- textDocument/codelens
-  -- local client = assert(lsp.get_client_by_id(_.data.client_id))
-  -- if client.supports_method('textDocument/codeLens') then
-  --   lsp.codelens.refresh({ bufnr = _.buf })
-  --   api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
-  --     callback = function(_) lsp.codelens.refresh({ bufnr = _.buf }) end,
-  --   })
-  -- end
+  local client = assert(lsp.get_client_by_id(_.data.client_id))
+  if client:supports_method('textDocument/codeLens') then
+    lsp.codelens.refresh { bufnr = _.buf }
+    local _f = u.lodash.debounce(200, function(_) lsp.codelens.refresh { bufnr = _.buf } end)
+    u.aug.lsp_codelens = {
+      { 'BufEnter', 'CursorHold', 'InsertLeave' },
+      { callback = _f },
+    }
+  end
 end
 
 Lsp.setup = function()
