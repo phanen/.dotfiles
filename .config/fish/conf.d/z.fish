25082442838a39e460deeef1b49df0a8ba404bb2
phanium <nxhsdu@qq.com>
2024-11-24 20:03:01 +0800

    chore(fish): cleanup

--- .config/fish/conf.d/z.fish
+++ .config/fish/conf.d/z.fish
@@ -1,21 +1,6 @@
-# database backend (external binary)
-# note: set -l cannot access by function
-if not command -q zoxide
-    echo 'no back end'
-    return
-end
+command -q zoxide; or return
 
-function __z_pwd
-    builtin pwd -L
-end
-
-function __z_db_add
-    command zoxide add $argv
-end
-
-# fish_indent: ignore end
 function __z_hook --on-variable PWD
-    # echo pwd $argv
     test -z "$fish_private_mode"
-    and __z_db_add -- (__z_pwd)
+    and command zoxide add -- (builtin pwd -L)
 end
