db7a9fa5e34b2edd524778efc5c93b00cd056ef6
phanium <nxhsdu@qq.com>
2024-12-09 15:29:12 +0800

    chore: upd

--- .config/fish/conf.d/fisher.fish
+++ .config/fish/conf.d/fisher.fish
@@ -1,3 +1,5 @@
+status is-interactive; or return
+
 function fisher --argument-names cmd --description "A plugin manager for Fish"
     set --query fisher_path || set --local fisher_path $__fish_config_dir
     set --local fisher_version 4.4.4
