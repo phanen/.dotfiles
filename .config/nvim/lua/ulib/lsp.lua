3127ca1b491f463a784bca876bba804f3c73884d
phanium <nxhsdu@qq.com>
2024-12-11 19:04:28 +0800

    feat: nightly lsp

--- .config/nvim/lua/ulib/lsp.lua
+++ .config/nvim/lua/ulib/lsp.lua
@@ -1,14 +1,5 @@
 local Lsp = {}
 
----@return lsp.ClientCapabilities
-Lsp.make_capabilities = function()
-  local ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-  local lsp_cap = lsp.protocol.make_client_capabilities()
-  local cmp_cap = ok and cmp_nvim_lsp.default_capabilities()
-  return u.merge(lsp_cap or {}, cmp_cap or {})
-end
----Lsp.make_capabilities = vim.func._memoize(nil, Lsp.make_capabilities, true)
-
 ---@autocmd
 Lsp.on = function(_)
   local n = map[0].n
@@ -17,12 +8,129 @@ Lsp.on = function(_)
   n['gh'] = function() u.pick.lsp_code_actions() end
   n.nowait['gr'] = function() u.pick.lsp_references() end -- note: nowait only apply to before mappings
 
+  -- TODO: on_clangd.. on_lua_ls...
+  -- e.g. lspconfig command feature, e.g. ClangdSwitchSourceHeader
+end
+
+---@autocmd
+Lsp.setup = function()
+  -- https://github.com/neovim/neovim/pull/31031
+
+  local cap = pcall(require, 'cmp_nvim_lsp')
+  if cap then lsp.config('*', { capablities = cap }) end
+
+  -- ensure mason path is prepended to PATH
+  local pylance = {
+    (true and fn.executable('delance-langserver') == 1) and 'delance-langserver'
+      or 'pyright-langserver',
+    '--stdio',
+  }
+
+  lsp.config('clangd', {
+    cmd = { 'clangd', '--clang-tidy', '--background-index', '--offset-encoding=utf-8' },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+    root_markers = { '.clangd', 'compile_commands.json' },
+  })
+
+  lsp.config('lua_ls', {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = {
+      '.luarc.json',
+      '.luarc.jsonc',
+      '.luacheckrc',
+      '.stylua.toml',
+      'stylua.toml',
+      'selene.toml',
+      'selene.yml',
+    },
+    -- Note this is ignored if the project has a .luarc.json
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = 'Replace',
+          -- postfix = '.', -- no string.method now
+          showWord = 'Disable',
+          workspaceWord = false,
+        },
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME,
+            '${3rd}/busted/library',
+            '${3rd}/luv/library',
+          },
+        },
+        runtime = { version = 'LuaJIT' },
+      },
+    },
+    on_attach = function(...) return u.misc.auto_lua_require(...) end,
+  })
+
+  lsp.enable {
+    -- 'clangd',
+    'lua_ls',
+  }
+
+  local l = require('lspconfig')
+
+  l.clangd.setup {}
+
+  l.pyright.setup { cmd = pylance } -- https://github.com/microsoft/pyright/blob/main/docs/configuration.md
+
+  l.gopls.setup {
+    settings = {
+      gopls = {
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+      },
+    },
+  }
+
+  l.ts_ls.setup {}
+
+  l.volar.setup {
+    on_attach = function(client, _) client.server_capabilities.documentFormattingProvider = false end,
+  }
+
+  l.vimls.setup {}
+
   vim.diagnostic.config {
     -- update_in_insert = true,
     float = { border = 'none' },
   }
 
   if true then return end
+  -- lsp folding
+  if false and lsp._folding_range then
+    vim.opt.sessionoptions:remove('folds')
+    vim.opt_local.fillchars = {
+      eob = ' ',
+      diff = '╱',
+      foldopen = '',
+      foldclose = '',
+      foldsep = '▕',
+    }
+    vim.o.foldlevel = 99
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    vim.wo.foldtext = 'v:lua.vim.lsp.foldtext()'
+    vim.wo.foldcolumn = '1'
+    api.nvim_create_autocmd('LspNotify', {
+      callback = function(args)
+        if args.data.method == 'textDocument/didOpen' then
+          lsp.foldclose('imports', vim.fn.bufwinid(args.buf))
+        end
+      end,
+    })
+  end
 
   -- https://github.com/lewis6991/dotfiles/blob/main/config%2Fnvim%2Flua%2Flewis6991%2Flsp.lua
   local method = 'textDocument/documentHighlight'
@@ -57,166 +165,6 @@ Lsp.on = function(_)
       { callback = _f },
     }
   end
-end
-
----@autocmd
-Lsp.setup = function()
-  -- ensure mason path is prepended to PATH
-  local l = require('lspconfig')
-
-  -- lsp folding
-  if false and lsp._folding_range then
-    vim.opt.sessionoptions:remove('folds')
-    vim.opt_local.fillchars = {
-      eob = ' ',
-      diff = '╱',
-      foldopen = '',
-      foldclose = '',
-      foldsep = '▕',
-    }
-    vim.o.foldlevel = 99
-    vim.wo.foldmethod = 'expr'
-    vim.wo.foldexpr = 'v:lua.vim.lsp.foldexpr()'
-    vim.wo.foldtext = 'v:lua.vim.lsp.foldtext()'
-    vim.wo.foldcolumn = '1'
-    api.nvim_create_autocmd('LspNotify', {
-      callback = function(args)
-        if args.data.method == 'textDocument/didOpen' then
-          lsp.foldclose('imports', vim.fn.bufwinid(args.buf))
-        end
-      end,
-    })
-  end
-
-  l.lua_ls.setup {
-    settings = {
-      Lua = {
-        completion = {
-          callSnippet = 'Replace',
-          -- postfix = '.', -- no string.method now
-          showWord = 'Disable',
-          workspaceWord = false,
-        },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-            '${3rd}/busted/library',
-            '${3rd}/luv/library',
-          },
-        },
-        runtime = { version = 'LuaJIT' },
-      },
-    },
-    on_attach = function(client, bufnr)
-      --- @param x string
-      --- @return string?
-      local function match_require(x)
-        -- WIP: match u.xx?
-        return x:match('require')
-          and (
-            x:match("require%s*%(%s*'([^.']+).*'%)")
-            or x:match('require%s*%(%s*"([^."]+).*"%)')
-            or x:match("require%s*'([^.']+).*'%)")
-            or x:match('require%s*"([^."]+).*"%)')
-          )
-      end
-
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if uv.fs_stat(path .. '/.luarc.json') or uv.fs_stat(path .. '/.luarc.jsonc') then
-          -- Updates to settings are igored if a .luarc.json is present
-          return
-        end
-
-        -- local fd = uv.fs_open(root_dir .. '/.luarc.json', 'r', 438)
-        -- local luarc = vim.json.decode(assert(vim.uv.fs_read(fd, vim.uv.fs_fstat(fd).size)))
-        -- if not (luarc.workspace and luarc.workspace.library) then return end
-      end
-
-      client.settings = u.merge({ Lua = { workspace = { library = {} } } }, client.settings)
-
-      --- @param first? integer
-      --- @param last? integer
-      local function on_lines(first, last)
-        local do_change = false
-
-        local lines = api.nvim_buf_get_lines(bufnr, first or 0, last or -1, false)
-        for _, line in ipairs(lines) do
-          local m = match_require(line)
-          if m then
-            for _, mod in ipairs(vim.loader.find(m, { patterns = { '', '.lua' } })) do
-              local lib = fs.dirname(mod.modpath)
-              local libs = client.settings.Lua.workspace.library
-              if not vim.tbl_contains(libs, lib) then
-                libs[#libs + 1] = lib
-                do_change = true
-              end
-            end
-          end
-        end
-
-        if do_change then
-          client.notify('workspace/didChangeConfiguration', { settings = client.settings })
-        end
-      end
-
-      api.nvim_buf_attach(bufnr, false, {
-        on_lines = function(_, _, _, first, _, last) on_lines(first, last) end,
-        on_reload = function() on_lines() end,
-      })
-      on_lines()
-    end,
-  }
-
-  l.clangd.setup {
-    cmd = {
-      'clangd',
-      '--background-index',
-      '--clang-tidy',
-      '--header-insertion=iwyu',
-    },
-  }
-
-  l.pyright.setup {
-    cmd = {
-      fn.executable('delance-langserver') == 1 and 'delance-langserver' or 'pyright-langserver',
-      '--stdio',
-    },
-    settings = {
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          useLibraryCodeForTypes = true,
-          diagnosticMode = 'openFilesOnly',
-        },
-      },
-    },
-  }
-
-  l.gopls.setup {
-    settings = {
-      gopls = {
-        hints = {
-          assignVariableTypes = true,
-          compositeLiteralFields = true,
-          compositeLiteralTypes = true,
-          constantValues = true,
-          functionTypeParameters = true,
-          parameterNames = true,
-          rangeVariableTypes = true,
-        },
-      },
-    },
-  }
-
-  l.ts_ls.setup {}
-
-  l.volar.setup {
-    on_attach = function(client, _) client.server_capabilities.documentFormattingProvider = false end,
-  }
-
-  l.vimls.setup {}
 end
 
 return Lsp
--- .config/nvim/lua/ulib/misc.lua
+++ .config/nvim/lua/ulib/misc.lua
@@ -219,4 +219,64 @@ Misc.lz_shada = function()
   return true
 end
 
+Misc.auto_lua_require = function(client, bufnr)
+  --- @param x string
+  --- @return string?
+  local function match_require(x)
+    -- WIP: match u.xx?
+    return x:match('require')
+      and (
+        x:match("require%s*%(%s*'([^.']+).*'%)")
+        or x:match('require%s*%(%s*"([^."]+).*"%)')
+        or x:match("require%s*'([^.']+).*'%)")
+        or x:match('require%s*"([^."]+).*"%)')
+      )
+  end
+
+  if client.workspace_folders then
+    local path = client.workspace_folders[1].name
+    if uv.fs_stat(path .. '/.luarc.json') or uv.fs_stat(path .. '/.luarc.jsonc') then
+      -- Updates to settings are igored if a .luarc.json is present
+      return
+    end
+
+    -- local fd = uv.fs_open(root_dir .. '/.luarc.json', 'r', 438)
+    -- local luarc = vim.json.decode(assert(vim.uv.fs_read(fd, vim.uv.fs_fstat(fd).size)))
+    -- if not (luarc.workspace and luarc.workspace.library) then return end
+  end
+
+  client.settings = u.merge({ Lua = { workspace = { library = {} } } }, client.settings)
+
+  --- @param first? integer
+  --- @param last? integer
+  local function on_lines(first, last)
+    local do_change = false
+
+    local lines = api.nvim_buf_get_lines(bufnr, first or 0, last or -1, false)
+    for _, line in ipairs(lines) do
+      local m = match_require(line)
+      if m then
+        for _, mod in ipairs(vim.loader.find(m, { patterns = { '', '.lua' } })) do
+          local lib = fs.dirname(mod.modpath)
+          local libs = client.settings.Lua.workspace.library
+          if not vim.tbl_contains(libs, lib) then
+            libs[#libs + 1] = lib
+            do_change = true
+          end
+        end
+      end
+    end
+
+    if do_change then
+      client.notify('workspace/didChangeConfiguration', { settings = client.settings })
+    end
+  end
+
+  api.nvim_buf_attach(bufnr, false, {
+    on_lines = function(_, _, _, first, _, last) on_lines(first, last) end,
+    on_reload = function() on_lines() end,
+  })
+  on_lines()
+end
+
 return Misc
