cfe7f3429e37a040c5472fc22d2b18452f1682b7
phanium <nxhsdu@qq.com>
2024-12-12 21:34:02 +0800

    refactor(term): spawn/send cmd

--- .config/nvim/lua/ulib/muxterm.lua
+++ .config/nvim/lua/ulib/muxterm.lua
@@ -2,15 +2,19 @@ local Term = u.term
 local HashList = u.hashlist
 
 local MuxTerm = {}
+
+MuxTerm.win = nil
+-- if needed, we can manage windows here...
+-- (share one win for multiple Terms, also without `WinLeave`)
+
 local slots = HashList {} ---@type HashList
 MuxTerm.slots = slots
 MuxTerm.curr = slots.head
-local i = 1
 
-local get_key = function()
-  local key = i
-  i = i + 1
-  return key
+MuxTerm.i = 0
+MuxTerm.get_key = function()
+  MuxTerm.i = MuxTerm.i + 1
+  return MuxTerm.i
 end
 
 -- 1. close term win on another tabpage
@@ -20,7 +24,6 @@ u.aug.muxterm_auto_close_float_win = {
   {
     pattern = 'term://*',
     callback = function(ev)
-      -- u.sysp('eeeeee')
       if vim.b[ev.buf].is_float_muxterm then
         if MuxTerm.curr.data then MuxTerm.curr.data:close() end
       end
@@ -28,85 +31,121 @@ u.aug.muxterm_auto_close_float_win = {
   },
 }
 
-local update_winbar = function(win)
+MuxTerm.winbar = function()
+  local bar = {}
+  slots:foreach(function(node)
+    -- local key = node.data.title or node.key
+    local hl = MuxTerm.curr == node and 'TabLineSel' or 'TabLine'
+    local n = #bar + 1
+    bar[n] = ('%%#%s# %s %%#TabLineFill#'):format(hl, n)
+  end)
+  return table.concat(bar)
+end
+
+MuxTerm.update_winbar = function(win)
   if not u.is.win_valid(win) then return end
   if vim.tbl_count(slots.hash) == 1 then
-    -- print('win2', win)
     vim.wo[win].winbar = nil
     return
   end
   if not vim.wo[win].winbar or vim.wo[win].winbar == '' then
-    -- print('win3', win)
     vim.wo[win].winbar = '%{%v:lua.u.muxterm.winbar()%}'
     return
   end
 end
 
-local switch_to = function(to)
+MuxTerm.size = function() return slots.size end
+
+---@param to HashNode
+---@param opts TermConfig?
+---@param should_open boolean? open or not after switch
+MuxTerm.switch_to = function(to, opts, should_open)
   if to == MuxTerm.curr then return end
-  MuxTerm.curr.data:close()
-  to.data:open()
-  update_winbar(to.data.win)
+  -- current node may have been deleted
+  if not MuxTerm.is_empty() then MuxTerm.curr.data:close() end
+  if to == slots.tail or to == slots.head then
+    MuxTerm.curr = to
+    return
+  end
+  if should_open ~= false then to.data:open(opts) end
+  MuxTerm.update_winbar(to.data.win)
   MuxTerm.curr = to
 end
 
-MuxTerm.winbar = function()
-  local bar = {}
-  slots:foreach(function(node)
-    local key = node.data.title or node.key
-    local hl = MuxTerm.curr == node and 'TabLineSel' or 'TabLine'
-    bar[#bar + 1] = ('%%#%s# %s %%#TabLineFill#'):format(hl, key)
-    -- { "%#TabLine# 1 %#TabLineFill#", "%#TabLine# 2 %#TabLineFill#", "%#TabLineSel# 3 %#TabLineFill#" }
-  end)
-  return table.concat(bar)
+MuxTerm.get_next = function()
+  return MuxTerm.curr.next ~= slots.tail and MuxTerm.curr.next or slots.head.next
 end
 
-local delete = function()
-  MuxTerm.slots:delete(MuxTerm.curr)
-  local next_term =
-    assert(MuxTerm.curr.next ~= MuxTerm.slots.tail and MuxTerm.curr.next or MuxTerm.slots.tail.prev)
-  -- MuxTerm.curr.data:close(true)
-  if next_term ~= slots.head then
-    next_term.data:open()
-    update_winbar(next_term.data.win)
+MuxTerm.get_prev = function()
+  return MuxTerm.curr.prev ~= slots.head and MuxTerm.curr.prev or slots.tail.prev
+end
+
+-- should switch to which node after current node deleted
+MuxTerm.get_near = function()
+  return MuxTerm.curr.next ~= slots.tail and MuxTerm.curr.next or slots.tail.prev
+end
+
+MuxTerm.cycle_next = function(opts) MuxTerm.switch_to(MuxTerm.get_next(), opts) end
+
+MuxTerm.cycle_prev = function(opts) MuxTerm.switch_to(MuxTerm.get_prev(), opts) end
+
+---@return boolean
+MuxTerm.is_empty = function() return MuxTerm.curr == slots.head end
+
+---@return boolean
+MuxTerm.is_focusd = function() return not MuxTerm.is_empty() and MuxTerm.curr.data:is_focusd() end
+
+---@param opts TermConfig?
+---then switch to it (currently, only start exec on the first focus...)
+MuxTerm.spawn = function(opts)
+  -- hook on term close
+  local delete = function()
+    slots:delete(MuxTerm.curr)
+    -- if current tab is open, then when it deleted, try open next
+    local curr_is_open = MuxTerm.is_focusd()
+    MuxTerm.switch_to(MuxTerm.get_near(), nil, curr_is_open)
   end
-  MuxTerm.curr = next_term
+
+  local on_exit = function()
+    if opts and opts.on_exit then opts.on_exit() end
+    delete()
+  end
+
+  local term = Term { on_exit = on_exit }
+  vim.b[term.buf].is_float_muxterm = true -- maybe we can also use filetype here
+  local node = { key = MuxTerm.get_key(), data = term }
+  slots:insert_after(MuxTerm.curr, node)
+  MuxTerm.cycle_next(opts)
 end
 
-MuxTerm.insert = function()
-  local term = Term {
-    auto_close = false, -- we handle all close procedure
-    on_exit = function() delete() end,
-  }
-  -- maybe we can also use filetype here
-  vim.b[term.buf].is_float_muxterm = true
-  slots:insert_after(MuxTerm.curr, { key = get_key(), data = term })
-end
-
-MuxTerm.insert_then_switch = function()
-  MuxTerm.insert()
-  MuxTerm.cycle_next()
-end
-
-MuxTerm.toggle = function()
-  -- no term now, create one
-  if MuxTerm.curr == slots.head then
-    MuxTerm.insert()
-    local next_term = assert(MuxTerm.curr.next)
-    next_term.data:open()
-    MuxTerm.curr = next_term
+---@param opts TermConfig?
+MuxTerm.open = function(opts)
+  if not MuxTerm.is_empty() then
+    MuxTerm.curr.data:open(opts)
     return
   end
-
-  MuxTerm.curr.data:toggle()
+  MuxTerm.spawn(opts)
 end
 
-MuxTerm.cycle_next = function()
-  switch_to(MuxTerm.curr.next ~= slots.tail and MuxTerm.curr.next or slots.head.next)
+MuxTerm.close = function()
+  if MuxTerm.is_empty() then return end
+  MuxTerm.curr.data:close()
 end
 
-MuxTerm.cycle_prev = function()
-  switch_to(MuxTerm.curr.prev ~= slots.head and MuxTerm.curr.prev or slots.tail.prev)
+---@param opts TermConfig?
+MuxTerm.toggle = function(opts)
+  if MuxTerm.is_focusd() then
+    MuxTerm.close()
+    return
+  end
+  MuxTerm.open(opts)
+end
+
+---@param cmd TermCmd
+MuxTerm.send = function(cmd)
+  MuxTerm.open()
+  vim.wait(30) -- hack: wait fish prompt...
+  MuxTerm.curr.data:send(cmd)
 end
 
 return MuxTerm
