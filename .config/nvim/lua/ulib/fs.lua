e7b79aa0efa834b8edef2d4ac830b120745ae0aa
phanium <nxhsdu@qq.com>
2024-12-12 21:31:24 +0800

    fix: just crash it

--- .config/nvim/lua/ulib/fs.lua
+++ .config/nvim/lua/ulib/fs.lua
@@ -2,10 +2,10 @@ local Fs = {}
 
 ---read file contents
 ---@param path string
----@return string?
+---@return string
 Fs.read_file = function(path)
   local file = io.open(path, 'r')
-  if not file then return end
+  if not file then error('Failed to open file: ' .. path) end
   local content = file:read('*a')
   file:close()
   return content
@@ -15,24 +15,19 @@ end
 ---@param path string
 ---@param content string
 ---@param flags string?
----@return boolean success
 Fs.write_file = function(path, content, flags)
   local file = io.open(path, flags or 'w')
-  if not file then return false end
+  if not file then error('Failed to open file: ' .. path) end
   file:write(content)
   file:close()
-  return true
 end
 
-Fs.must_read_file = function(path) return assert(Fs.read_file(path)) end
+Fs.filecopy = function(from, to) return assert(fn.filecopy(from, to) == 0) end
 
-Fs.must_write_file = function(path, content, flags)
-  return assert(Fs.write_file(path, content, flags) == 0)
-end
-
-Fs.must_filecopy = function(from, to) return assert(fn.filecopy(from, to) == 0) end
-
-Fs.must_writefile = function(lines, path, flags)
+---@param path string
+---@param lines string[]
+---@param flags string?
+Fs.write_file_by_lines = function(path, lines, flags)
   if flags then return assert(fn.writefile(lines, path, flags) == 0) end
   return assert(fn.writefile(lines, path) == 0)
 end
@@ -64,18 +59,18 @@ end
 Fs.read_json = function(path, opts)
   opts = opts or {}
   local content = Fs.read_file(path)
-  local ok, tbl = pcall(vim.json.decode, content, opts)
-  return ok and tbl or {}
+  local tbl = vim.json.decode(content, opts)
+  return tbl
 end
 
 ---Write json contents
 ---@param path string
 ---@param tbl table
----@return boolean success
-Fs.write_json = function(path, tbl)
-  local ok, content = pcall(vim.json.encode, tbl)
-  if not ok then return false end
-  return Fs.write_file(path, content)
+---@param opts table? same option table as `vim.json.decode()`
+Fs.write_json = function(path, tbl, opts)
+  opts = opts or {}
+  local content = vim.json.encode(tbl, opts)
+  Fs.write_file(path, content)
 end
 
 return Fs
