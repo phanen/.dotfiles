cfac0334e29d08fabd0737dc858bbd51709275a7
phanium <nxhsdu@qq.com>
2024-12-17 19:14:19 +0800

    fix: fish cmp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,7 +20,7 @@ set fisher_path $__fish_user_data_dir/fisher
 
 # priority: user > plugin > system-wide
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
-set fish_complete_path fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
 for file in $fisher_path/conf.d/*.fish
     source $file
 end
