4657a54b423bc4d1c3e2d3762d542d865fc9d7b5
phanium <nxhsdu@qq.com>
2024-12-17 22:00:13 +0800

    fix: don't preview fugitive

--- .config/nvim/lua/plugs/nvim-bqf.lua
+++ .config/nvim/lua/plugs/nvim-bqf.lua
@@ -6,13 +6,11 @@ return {
     preview = {
       border = g.border,
       extra_opts = { '--bind', 'ctrl-q:toggle-all' },
-      -- should_preview_cb = function(bufnr, _)
-      --   local bufname = api.nvim_buf_get_name(bufnr)
-      --   local fsize = fn.getfsize(bufname)
-      --   if bufname:match '^fugitive://' then return false end
-      --   if fsize > 100 * 1024 then return false end
-      --   return true
-      -- end,
+      should_preview_cb = function(bufnr, _)
+        local bufname = api.nvim_buf_get_name(bufnr)
+        local fsize = fn.getfsize(bufname)
+        return fsize < 100 * 1024 and not bufname:match '^fugitive://'
+      end,
     },
   },
 
