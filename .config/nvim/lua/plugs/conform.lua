d203ff6664b911909bcb90d7b0f2281d5b5b21e3
phanium <nxhsdu@qq.com>
2024-12-25 10:47:29 +0800

    fix: conform wrong flags

--- .config/nvim/lua/plugs/conform.lua
+++ .config/nvim/lua/plugs/conform.lua
@@ -3,15 +3,16 @@ return {
   cmd = 'ConformInfo',
   opts = {
     formatters_by_ft = {
-      fish = { 'fish_indent', '--only-indent' },
+      fish = { 'fish_indent' },
       lua = { 'stylua' },
       python = { 'ruff_fix', 'ruff_format' },
+      dart = { 'dart_format' },
       sh = { 'shfmt', 'shellcheck' },
       xml = { 'xmlformat' },
       toml = { 'taplo' },
       c = { 'clang-format' },
       cpp = { 'clang-format' },
-      tex = { 'latexindent', '--GCString' }, -- for GCString feature, install `perl-unicode-linebreak` as extra dependency (for arch)
+      tex = { 'latexindent' },
       bib = { 'bibtex-tidy' },
       css = { 'prettier' },
       html = { 'prettier' },
@@ -25,25 +26,11 @@ return {
       typescriptreact = { 'prettier' },
       vue = { 'prettier' },
       yaml = { 'prettier' },
-      -- markdown = { 'injected' },
+      markdown = { 'injected' },
       help = { 'injected' },
     },
     formatters = {
-      shfmt = {
-        command = 'shfmt',
-        args = function(_, ctx)
-          local args = { '-filename', '$FILENAME' }
-          local has_editorconfig = fs.find('.editorconfig', { path = ctx.dirname, upward = true })[1]
-            ~= nil
-
-          -- If there is an editorconfig, don't pass any args because shfmt will apply settings from there
-          -- when no command line args are passed.
-          -- if not has_editorconfig and vim.bo[ctx.buf].expandtab then
-          -- buggy?
-          if vim.bo[ctx.buf].expandtab then vim.list_extend(args, { '-i', ctx.shiftwidth }) end
-          return args
-        end,
-      },
+      latexindent = { inherit = true, prepend_args = { '--GCString' } }, -- need perl-unicode-linebreak
     },
   },
 }
