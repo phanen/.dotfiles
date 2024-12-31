470e2c248e64b7d8510745d02fbdd9bf8824053b
phanium <91544758+phanen@users.noreply.github.com>
2024-12-31 10:49:34 +0800

    fix: just back compat
    tired for handling nix derivations

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,31 @@
+if test $__fish_initialized -lt 3800
+    bind \ci _fifc
+    bind \ce _eol_or_fzf
+    bind \c\; "exec fish"
+    bind \cf _forward_or_fzf
+    bind \cg '_empty_then "yazi;commandline -f repaint" _fish_lookup'
+    bind \ch '_empty_then "__fish_echo tokei" "_autopair_backspace"'
+    bind \cj nextd-or-forward-word
+    bind \cl '_empty_trim_then "_clean;commandline -f repaint" "commandline -f kill-token"'
+    bind \co prevd-or-backward-word
+    bind \cq '_empty_trim_then "lazygit;commandline -f repaint" fish_clipboard_copy'
+    bind \cr atuin_search
+    bind \cs '_empty_then nvim __fish_man_page'
+    bind \ct 'commandline -i -- (_fzf_files)'
+    bind \cu '_empty_then "htop;commandline -f repaint" "commandline -f backward-kill-line"'
+    bind \cw '_empty_then "__fish_echo eza -lh --hyperlink" "commandline -f backward-kill-path-component"'
+    bind \cm 'nvim -- (commandline -t)'
+
+    bind \e\; 'exec fish'
+    bind \ei 'tmux a &>/dev/null || tmux &>/dev/null || tmux det'
+    bind \el clear-screen
+    bind \ew 'fish_key_reader -c'
+
+    bind \eg commandline
+    # bind \r k_enter
+    return
+end
+
 bind ctrl-i _fifc
 bind ctrl-e _eol_or_fzf
 bind ctrl-\; "exec fish"
@@ -6,7 +34,8 @@ bind ctrl-g '_empty_then "yazi;commandline -f repaint" _fish_lookup'
 bind ctrl-h '_empty_then "__fish_echo tokei" "_autopair_backspace"'
 bind ctrl-j nextd-or-forward-word
 # TODO: if at the beginning of word, also kill space...
-bind ctrl-l '_empty_trim_then "_clean;commandline -f repaint" "commandline -f kill-bigword"'
+# bind ctrl-l '_empty_trim_then "_clean;commandline -f repaint" "commandline -f kill-bigword"'
+bind ctrl-l '_empty_trim_then "_clean;commandline -f repaint" "commandline -f kill-token"'
 bind ctrl-o prevd-or-backward-word
 bind ctrl-q '_empty_trim_then "lazygit;commandline -f repaint" fish_clipboard_copy'
 bind ctrl-r atuin_search
