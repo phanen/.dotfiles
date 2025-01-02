6809d24649d47fe2883169063535af99ae23cf6a
phanium <91544758+phanen@users.noreply.github.com>
2025-01-02 16:42:15 +0800

    fix: remove patch

--- .config/nvim/lua/plugs/edit/multicursor.lua
+++ .config/nvim/lua/plugs/edit/multicursor.lua
@@ -4,7 +4,11 @@ g.VM_maps = {
   ['Add Cursor Down'] = '<a-n>',
   ['Add Cursor Up'] = '<a-p>',
   ['Select All'] = '<leader>N',
+  ['I BS'] = '<c-h>',
+  ['I Home'] = '<c-a>',
+  ['I End'] = '<c-e>',
 }
+
 g.VM_mouse_mappings = 1
 
 return { 'mg979/vim-visual-multi', event = 'VeryLazy' }
