aea6f72350e82309c2bd15af0a152a8dfc863a07
phanium <91544758+phanen@users.noreply.github.com>
2025-01-07 20:07:23 +0800

    fix: select devices

--- .bin/adb-unlock.sh
+++ .bin/adb-unlock.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-adb shell input keyevent KEYCODE_WAKEUP
-sleep 1
-adb shell input swipe 500 1000 500 200
-adb shell input tap 250 1080
-adb shell input tap 250 1360
-adb shell input tap 550 1080
-adb shell input tap 550 1360
-adb shell input tap 820 1880
+for i in "$@"; do
+  case $i in
+  -w | --wake-only) WAKE_ONLY=1 ;;
+
+    # https://unix.stackexchange.com/questions/577603/possible-to-match-multiple-conditions-in-one-case-statement
+  -s) HOST="$2" ;;&
+  *) ADB_ARGS+=("$i") ;;
+  esac
+done
+
+adb connect "$HOST"
+adb "${ADB_ARGS[@]}" shell input keyevent KEYCODE_WAKEUP
+[ -n "$WAKE_ONLY" ] && exit 0
+sleep .3
+
+adb "${ADB_ARGS[@]}" shell input swipe 500 1200 500 100
+sleep .3
+
+adb "${ADB_ARGS[@]}" shell input tap 250 1080
+adb "${ADB_ARGS[@]}" shell input tap 250 1360
+adb "${ADB_ARGS[@]}" shell input tap 550 1080
+adb "${ADB_ARGS[@]}" shell input tap 550 1360
+adb "${ADB_ARGS[@]}" shell input tap 820 1880
+
+if ! pgrep scrcpy; then
+  scrcpy -Sw "${ADB_ARGS[@]}"
+fi
